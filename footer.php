3de1ee1b81f7c87f2e94296cb011a8ccc644134f
Patrick Best <patrickbest@patrickbest.com>
2014-12-12 20:26:23 -0600

    add analytics code

--- footer.php
+++ footer.php
@@ -24,6 +24,17 @@
 <section id="scripts">
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery-2.1.1.min.js"><\/script>')</script>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-37044100-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+
 <?php
 wp_footer();
 if(is_single()) {
