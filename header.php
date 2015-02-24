f8d9aff46f6720485829ddf5680499248eae7cc6
Patrick Best <patrickbest@patrickbest.com>
2015-02-23 19:45:00 -0600

    update twitter card meta

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta name="twitter:site" value="@">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" value="@novaramedia">
   <?php if (have_posts()):while(have_posts()):the_post();
     $excerpt = get_the_excerpt();
     if(has_post_thumbnail()) {
@@ -17,25 +18,32 @@
     if (!empty($thumb)) {
   ?>
   <meta property="og:image" content="<?php echo $thumb['0'] ?>" />
+  <meta name="twitter:image:src" content="<?php echo $thumb['0'] ?>">
     <?php } else { ?>
   <meta property="og:image" content="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.png" />
+  <meta name="twitter:image:src" content="<?php bloginfo('stylesheet_directory'); ?>/img/favicon.png">
     <?php }
     if (is_home()) { ?>
   <meta property="og:title" content="<?php bloginfo('name'); ?>" />
+  <meta name="twitter:title" content="<?php bloginfo('name'); ?>">
   <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
   <meta property="og:type" content="website" />
   <meta property="og:description" content="<?php bloginfo('description'); ?>" />
-  <meta name="twitter:card" value="<?php bloginfo('description'); ?>">
+  <meta name="twitter:description" content="<?php bloginfo('description'); ?>">
 <?php } elseif (is_single()) { ?>
   <meta property="og:url" content="<?php the_permalink() ?>"/>
   <meta property="og:title" content="<?php single_post_title(''); ?>" />
+  <meta name="twitter:title" content="<?php single_post_title(''); ?>">
   <meta property="og:description" content="<?php echo $excerpt ?>" />
+  <meta name="twitter:description" content="<?php echo $excerpt ?>">
   <meta property="og:type" content="article" />
   <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
 <?php } else { ?>
   <meta property="og:title" content="<?php single_post_title(''); ?>" />
+  <meta name="twitter:title" content="<?php single_post_title(''); ?>">
   <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
   <meta property="og:description" content="<?php bloginfo('description'); ?>" />
+  <meta name="twitter:description" content="<?php bloginfo('description'); ?>">
   <meta property="og:type" content="website" />
 <?php } ?>
 
