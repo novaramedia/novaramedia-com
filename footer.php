72fad3f8a49daddb329fdd5229fd3f7518ad3438
Patrick Best <patrickbest@patrickbest.com>
2016-05-16 18:20:34 -0500

    basic scaffold

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
     <footer id="footer">
+      <div class="container">
+        <div class="col col6">
+          About, etc
+        </div>
+        <div class="col col6">
+          Main cats
+        </div>
+        <div class="col col6">
+          Social media
+        </div>
+        <div class="col col6">
+          Subcats
+        </div>
+      </div>
     </footer>
 
   </section>
