9034b4cd69db11f1bbfa91cc0c96c400c7cb6278
Patrick Best <patrickbest@patrickbest.com>
2016-05-16 20:45:03 -0500

    scaffold search

--- searchform.php
+++ searchform.php
@@ -1,10 +1,12 @@
-<div class="container">
-  <div class="row">
-    <div class="col col24">
-      <form role="search" method="get" id="searchform" class="searchform" action="http://local.novaramedia.com/">
-        <input id="search-input" type="text" value="" name="s" id="s">
-        <input type="submit" id="searchsubmit" value="Search">
-      </form>
+<div class="background-black padding-top-basic padding-bottom-basic">
+  <div class="container">
+    <div class="row">
+      <div class="col col24">
+        <form role="search" method="get" id="search-form" class="padding-small u-cf" action="http://local.novaramedia.com/">
+          <input id="search-input" class="font-italic padding-tiny" type="text" value="" name="s" id="s">
+          <button type="submit" id="search-submit" class="padding-tiny background-white font-color-black"><i class="icon-search"></i></button>
+        </form>
+      </div>
     </div>
   </div>
 </div>
@@ -16,20 +18,22 @@ $tags = get_terms( 'post_tag', array(
 
 if ( ! empty( $tags ) ) {
 ?>
-<div id="tag-suggestions" class="container">
-  <div class="row">
-    <div class="col col6">
-      <h4>Tag suggestions</h4>
-    </div>
-    <div class="col col18">
-      <ul id="suggested-tags">
+<div id="tag-suggestions" class="background-light-gray font-color-white padding-top-basic padding-bottom-basic">
+  <div class="container">
+    <div class="row">
+      <div class="col col6">
+        <h4>Tag suggestions</h4>
+      </div>
+      <div class="col col18">
+        <ul id="suggested-tags">
 <?php
   foreach ( $tags as $tag ) {
     $tag_link = get_term_link($tag->term_id);
     echo '<li class="suggested-tag" data-tag="' . $tag->slug . '"><a href="' . $tag_link . '">' . $tag->name . '</a></li>';
   }
 ?>
-      </ul>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
