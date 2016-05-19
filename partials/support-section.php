97e14795a0bf13383955875060c7a34b6aa4334e
Patrick Best <patrickbest@patrickbest.com>
2016-05-19 13:43:26 -0500

    scaffold section

---
+++ partials/support-section.php
@@ -0,0 +1,52 @@
+<div class="support-section background-red font-color-white margin-top-basic margin-bottom-basic padding-top-mid padding-bottom-mid">
+  <div class="container">
+    <div class="row margin-bottom-small">
+      <div class="col col24">
+        <h4>Support Us</h4>
+      </div>
+    </div>
+
+    <div class="row margin-bottom-small font-bold">
+      <div class="col col12">
+        <p>Become a subscriber and support Novara Media from £1 per month:</p>
+      </div>
+      <div class="col col12">
+        <p>Or you can give us a one-off donation:</p>
+      </div>
+    </div>
+
+    <div class="row">
+
+      <form action="https://payment.novaramedia.com/subscription">
+
+        <div class="col col3">
+          <div class="support-form-holder support-form-value-holder">
+            £<span class="support-form-value">1</span> / month
+          </div>
+        </div>
+        <div class="col col9">
+          <div class="support-form-holder support-form-slider-holder">
+            <input class="support-form-slider" type="range" value="1" min="1" max="25" step="1" name="amount" /> £££ <input class="support-form-submit" type="submit" value="Go" />
+          </div>
+        </div>
+
+      </form>
+
+      <form action="https://payment.novaramedia.com/oneoff">
+
+        <div class="col col3">
+          <div class="support-form-holder support-form-value-holder">
+            £<span class="support-form-value">5</span>
+          </div>
+        </div>
+        <div class="col col9">
+          <div class="support-form-holder support-form-slider-holder">
+            <input class="support-form-slider" type="range" value="5" min="1" max="250" step="1" name="amount" /> £££ <input class="support-form-submit" type="submit" value="Go" />
+          </div>
+        </div>
+
+      </form>
+
+    </div>
+  </div>
+</div>
