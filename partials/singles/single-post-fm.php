3d47d0c9c9a2bc789b72cf48ce20a711c882e15d
cas <casska@gmail.com>
2016-05-19 18:21:26 -0500

    fix ugly if condition to check if powered by resonance

--- partials/singles/single-post-fm.php
+++ partials/singles/single-post-fm.php
@@ -33,14 +33,12 @@
         <iframe src="https://w.soundcloud.com/player/?url=<?php echo urlencode($meta['_cmb_sc'][0]); ?>" width="100%" height="200" scrolling="no" frameborder="no"></iframe>
 
       <?php
-        if (!empty($meta['_cmb_is_resonance'])) {
-          if ($meta['_cmb_is_resonance'][0]) {
+        if (!empty($meta['_cmb_is_resonance']) && $meta['_cmb_is_resonance'][0]) {
       ?>
         <div class="font-mono font-smaller">
         	<a target=_blank href="http://resonancefm.com/">powered by: Resonance FM</a>
         </div>
       <?php
-          }
         }
       } else {
         echo 'Someone messed up :/';
