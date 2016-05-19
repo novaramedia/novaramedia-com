9768bd485869d1f6c01b72c5db733b5a3ad79c48
Patrick Best <patrickbest@patrickbest.com>
2016-05-19 13:55:56 -0500

    style support section

--- partials/support-section.php
+++ partials/support-section.php
@@ -6,7 +6,7 @@
       </div>
     </div>
 
-    <div class="row margin-bottom-small font-bold">
+    <div class="row margin-bottom-tiny font-bold">
       <div class="col col12">
         <p>Become a subscriber and support Novara Media from £1 per month:</p>
       </div>
@@ -17,30 +17,30 @@
 
     <div class="row">
 
-      <form action="https://payment.novaramedia.com/subscription">
+      <form class="support-form" action="https://payment.novaramedia.com/subscription">
 
         <div class="col col3">
-          <div class="support-form-holder support-form-value-holder">
+          <div class="support-form-holder u-flex-center">
             £<span class="support-form-value">1</span> / month
           </div>
         </div>
         <div class="col col9">
-          <div class="support-form-holder support-form-slider-holder">
+          <div class="support-form-holder u-flex-center">
             <input class="support-form-slider" type="range" value="1" min="1" max="25" step="1" name="amount" /> £££ <input class="support-form-submit" type="submit" value="Go" />
           </div>
         </div>
 
       </form>
 
-      <form action="https://payment.novaramedia.com/oneoff">
+      <form class="support-form" action="https://payment.novaramedia.com/oneoff">
 
         <div class="col col3">
-          <div class="support-form-holder support-form-value-holder">
+          <div class="support-form-holder u-flex-center">
             £<span class="support-form-value">5</span>
           </div>
         </div>
         <div class="col col9">
-          <div class="support-form-holder support-form-slider-holder">
+          <div class="support-form-holder u-flex-center">
             <input class="support-form-slider" type="range" value="5" min="1" max="250" step="1" name="amount" /> £££ <input class="support-form-submit" type="submit" value="Go" />
           </div>
         </div>
