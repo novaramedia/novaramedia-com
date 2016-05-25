ab10912ea2435fa95d96876f0b46a682e2e4b365
Patrick Best <patrickbest@patrickbest.com>
2016-05-24 20:18:43 -0500

    disable archives

--- lib/post-types.php
+++ lib/post-types.php
@@ -42,7 +42,7 @@ function notices_post_type() {
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
-		'has_archive'           => true,
+		'has_archive'           => false,
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
