52fdcd2f15886f549ea6fa97d016022e9f96d62f
Patrick Best <patrickbest@patrickbest.com>
2016-05-26 17:41:04 -0500

    add missing thumb size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -8,6 +8,8 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
+  add_image_size( 'col4', 208, 9999, false );
+
   add_image_size( 'col8-16to9', 432, 243, true );
 
   add_image_size( 'col24-wire-crop', 1328, 400, true );
