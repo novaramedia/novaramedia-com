b1e791cadc5ef0d2594fb153da5f840b0b93811b
Patrick Best <patrickbest@patrickbest.com>
2016-06-09 19:52:25 -0500

    fix gradient overlay for post col12 layout

--- partials/post-layouts/post-col12.php
+++ partials/post-layouts/post-col12.php
@@ -6,7 +6,7 @@
   <article <?php post_class('col col12'); ?> id="post-<?php the_ID(); ?>">
 
     <div class="post-col12-image">
-    <?php the_post_thumbnail('col12-1to2point3', array('class' => 'replated-post-thumbnail')); ?>
+    <?php the_post_thumbnail('col12-1to2point3', array('class' => 'u-display-block')); ?>
 
       <div class="post-col12-text font-color-white">
         <h2 class="js-fix-widows"><?php the_title(); ?></h2>
