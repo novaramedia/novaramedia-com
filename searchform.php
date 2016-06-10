e337164b0c28eb0e30344a6ac94cfb5dae977cd5
Patrick Best <patrickbest@patrickbest.com>
2016-06-10 14:53:05 -0500

    fix search query

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col col24">
-        <form role="search" method="get" id="search-form" class="padding-small u-cf" action="http://local.novaramedia.com/">
+        <form role="search" method="get" id="search-form" class="padding-small u-cf" action="<?php echo site_url(); ?>">
           <input id="search-input" class="font-italic padding-tiny" type="text" value="" name="s" id="s">
           <button type="submit" id="search-submit" class="padding-tiny background-white font-color-black"><i class="icon-search"></i></button>
         </form>
