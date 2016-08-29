a051169afabf5cc2aa63cae6d6010f3f83b29ef7
Patrick Best <patrickbest@patrickbest.com>
2016-08-29 18:37:52 -0500

    fix wp admin bug with lazysizes

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -13,21 +13,25 @@ add_filter('embed_oembed_html', 'my_embed_oembed_html', 99, 4);
 // Custom img attributes to be compatible with lazysize
 function add_lazysize_on_srcset($attr) {
 
-  // Add lazysize class
-  $attr['class'] .= ' lazyload';
+  if (!is_admin()) {
 
-  if (isset($attr['srcset'])) {
-    // Add lazysize data-srcset
-    $attr['data-srcset'] = $attr['srcset'];
-    // Remove default srcset
-    unset($attr['srcset']);
-  } else {
-    // Add lazysize data-src
-    $attr['data-src'] = $attr['src'];
+    // Add lazysize class
+    $attr['class'] .= ' lazyload';
+
+    if (isset($attr['srcset'])) {
+      // Add lazysize data-srcset
+      $attr['data-srcset'] = $attr['srcset'];
+      // Remove default srcset
+      unset($attr['srcset']);
+    } else {
+      // Add lazysize data-src
+      $attr['data-src'] = $attr['src'];
+    }
+
+    // Set default to white blank
+    $attr['src'] = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+
   }
-
-  // Set default to white blank
-  $attr['src'] = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
   return $attr;
 
