26f099c61a56ee9861b9f6da80d99dd9a54e5052
Patrick Best <patrickbest@patrickbest.com>
2017-04-08 21:05:32 -0500

    main content layout

--- single-event.php
+++ single-event.php
@@ -10,9 +10,29 @@ get_header();
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
+
+    $timestamp = get_post_meta( $post->ID, '_cmb_time', true );
+
+    if ($timestamp) {
+      $time = new \Moment\Moment('@' . $timestamp);
+    } else {
+      $time = new \Moment\Moment('@' . get_the_time('U'));
+    }
+
+    $venue_name = get_post_meta( $post->ID, '_cmb_venue_name', true );
+    $venue_postcode = get_post_meta( $post->ID, '_cmb_venue_postcode', true );
+
+    $speakers = get_post_meta( $post->ID, '_cmb_speakers', true );
+    $host = get_post_meta( $post->ID, '_cmb_host', true );
+
+    $is_sold_out = get_post_meta( $post->ID, '_cmb_tickets_sold_out', true );
+    $tickets_url = get_post_meta( $post->ID, '_cmb_tickets', true );
+
+    $gallery = get_post_meta( $post->ID, '_cmb_gallery', true );
+
 ?>
   <article id="event">
-    <div class="container">
+    <div class="container margin-bottom-large">
       <div class="row margin-bottom-basic">
         <div class="col col24">
           <h4>IRL</h4>
@@ -20,7 +40,7 @@ if( have_posts() ) {
       </div>
       <div class="row margin-bottom-basic">
         <div class="col col24 text-align-center">
-          <h1>{Event Date}: <?php the_title(); ?></h1>
+          <h1><?php echo $time->format('j'); ?><sup><?php echo $time->format('S'); ?></sup><?php echo $time->format(' F Y'); ?>: <?php the_title(); ?></h1>
         </div>
       </div>
       <div class="row margin-bottom-basic">
@@ -32,24 +52,84 @@ if( have_posts() ) {
       <div class="row margin-bottom-basic">
         <div class="col col2"></div>
         <div class="col col8">
-          <h5>Time:</h5>
+          <div class="margin-bottom-small">
+            <h5 class="margin-bottom-small">Time:</h5>
+            <h3><?php echo $time->format('j'); ?><sup><?php echo $time->format('S'); ?></sup><?php echo $time->format(' F Y'); ?></h3>
+            <h3><?php echo $time->format('H:i'); ?></h3>
+          </div>
+        <?php
+          if ($venue_name) {
+        ?>
+          <div class="margin-bottom-small">
+            <h5 class="margin-bottom-small">Venue:</h5>
 
-          <h5>Venue:</h5>
+            <h3><?php echo $venue_name; ?></h3>
+        <?php
+            if ($venue_postcode) {
+        ?>
+            <h3><a href="https://www.google.com/maps/search/<?php echo urlencode($venue_postcode); ?>" target="_blank" rel="nofollow"><?php echo $venue_postcode; ?></a></h3>
+        <?php
+            }
+        ?>
+          </div>
+        <?php
+          }
+        ?>
 
-          <h5>Speakers:</h5>
+        <?php
+          if ($speakers) {
+        ?>
+          <div class="margin-bottom-small">
+            <h5 class="margin-bottom-small">Speakers:</h5>
 
-          <h5>Host:</h5>
+        <?php
+          foreach ($speakers as $speaker) {
+        ?>
+            <h3><?php echo $speaker; ?></h3>
+        <?php
+          }
+        ?>
+          </div>
+        <?php
+          }
+        ?>
+
+        <?php
+          if ($host) {
+        ?>
+          <div class="margin-bottom-small">
+            <h5 class="margin-bottom-small">Host:</h5>
+
+            <h3><?php echo $host; ?></h3>
+          </div>
+        <?php
+          }
+        ?>
+
+        <?php
+          if ($is_sold_out) {
+        ?>
+            <h4>Sold Out!</h4>
+        <?php
+          }
+        ?>
         </div>
-        <div class="col col12">
+        <div class="col col12 text-copy font-italic">
           <?php the_content(); ?>
         </div>
       </div>
+      <?php
+        if (!$is_sold_out && $tickets_url) {
+      ?>
       <div class="row margin-bottom-basic">
         <div class="col col2"></div>
         <div class="col col20">
-          Tickets
+          <a href="<?php echo $tickets_url; ?>" target="_blank" rel="nofollow" class="button"><h4>Buy Tickets</h4></a>
         </div>
       </div>
+      <?php
+        }
+      ?>
     </div>
 
     <div class="background-black font-color-white padding-top-mid padding-bottom-mid">
