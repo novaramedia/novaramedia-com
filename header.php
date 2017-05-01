e3c34a902b0d30d8473649971e5ada1c81b32638
Patrick Best <patrickbest@patrickbest.com>
2017-04-30 23:18:38 -0500

    update links to support

--- header.php
+++ header.php
@@ -74,7 +74,7 @@
               <ul class="header-menu u-inline-list">
                 <li><a href="<?php echo site_url('about/'); ?>">About</a></li>
                 <li><a href="<?php echo site_url('pitching/'); ?>">Pitching</a></li>
-                <li><a href="http://support.novaramedia.com">Support Us</a></li>
+                <li><a href="<?php echo site_url('support/'); ?>">Support Us</a></li>
                 <li><a href="<?php echo site_url('api/'); ?>">API</a></li>
               </ul>
             </div>
