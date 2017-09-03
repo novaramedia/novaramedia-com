21a83b4ba4c90b707aabbd27c4158b5bc66e298c
Patrick Best <patrickbest@patrickbest.com>
2017-09-03 12:51:00 +0100

    Hotedit: Add subscription profile link in footer

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
               <li><a href="<?php echo site_url('about/'); ?>">About</a></li>
               <li><a href="<?php echo site_url('pitching/'); ?>">Pitching</a></li>
               <li><a href="<?php echo site_url('support/'); ?>">Support Us</a></li>
+              <li><a href="https://payment.novaramedia.com/profile">Manage subscription</a></li>
               <li><a href="<?php echo site_url('api/'); ?>">API</a></li>
               <li><a href="http://podcast.novaramedia.com">Podcast</a></li>
               <li><a href="itpc://feeds.feedburner.com/NovaraMediaPodcast">Subscribe in iTunes</a></li>
