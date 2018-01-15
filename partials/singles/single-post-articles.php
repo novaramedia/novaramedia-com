e9f0b419827448efa18e155323ff9c4c78c6b745
varffs <patrickbest@Patricks-MacBook-Pro.local>
2018-01-15 17:33:08 +0000

    Hotupdate: Remove CC licence

--- partials/singles/single-post-articles.php
+++ partials/singles/single-post-articles.php
@@ -51,7 +51,6 @@
       }
       ?>
       <p id="single-articles-publication-date">Published <?php the_time('jS F Y'); ?></p>
-      <p>This work by Novara Media is licenced under a Creative Commons Attribution-ShareAlike 4.0 International Licence</p>
     </div>
   </div>
 </div>
