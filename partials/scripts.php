c60465e6b43aa90a1248d6bf30d1ea40568add69
varffs <patrick@interglobal.vision>
2020-02-18 20:20:47 -0600

    Update jquery version

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,6 +1,6 @@
 <section id="scripts">
-  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="<?php bloginfo('stylesheet_directory'); ?>/js/vendor/jquery-3.4.1.min.js"><\/script>')</script>
   <?php
     wp_footer();
 
