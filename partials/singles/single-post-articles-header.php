1bd3e13f8c2203de3d46b4c145f63f1376866eac
varffs <patrick@interglobal.vision>
2020-05-25 20:34:42 +0100

    Refactor: Single article header
    - add publish date to header
    - add possible soundcloud embed
    - improve layout

--- partials/singles/single-post-articles-header.php
+++ partials/singles/single-post-articles-header.php
@@ -17,7 +17,7 @@
 
 <div class="row margin-bottom-small">
   <div class="col col3"></div>
-  <div class="col col10 mobile-margin-bottom-tiny">
+  <div class="col col9 mobile-margin-bottom-tiny">
     <h3>by <?php
       if (!empty($meta['_cmb_author_twitter'])) {
         echo '<a id="single-articles-author" target="_blank" href="https://twitter.com/' . $meta['_cmb_author_twitter'][0] . '">';
@@ -40,23 +40,40 @@
         echo '</a>';
       }
     ?>
+    <h6 class="margin-top-micro"><?php the_time('jS F Y'); ?></h6>
   </div>
-  <div class="col col10 margin-top-micro font-smaller">
+  <div class="col col9 margin-top-micro font-smaller">
     <ul class="inline-action-list">
     <?php
       if (!empty($meta['_igv_reading_time'])) {
         echo '<li>Estimated read time: ';
         if ($meta['_igv_reading_time'][0] > 1) {
-          echo $meta['_igv_reading_time'][0] . 'mins';
+          echo $meta['_igv_reading_time'][0] . ' mins';
         } else {
-          echo $meta['_igv_reading_time'][0] . 'min';
+          echo $meta['_igv_reading_time'][0] . ' min';
         }
         echo '</li> ';
       }
     ?>
+    </ul>
+    <ul class="inline-action-list margin-top-micro">
       <li><?php render_tweet_link($share_url, $post->post_title, 'Tweet Article'); ?></li>
       <li><?php render_facebook_share_link($share_url); ?></li>
       <li><div class="kindleWidget" style="display:inline-block;cursor:pointer;white-space:nowrap;">Send to Kindle</div></li>
     </ul>
   </div>
 </div>
+
+<?php
+  if (!empty($meta['_cmb_sc'][0])) {
+?>
+<div class="row margin-top-basic margin-bottom-small">
+  <div class="col col3"></div>
+  <div class="col col18 mobile-margin-bottom-tiny">
+    <p class="font-smaller">Listen to this article as audio:</p>
+    <iframe src="https://w.soundcloud.com/player/?url=<?php echo urlencode($meta['_cmb_sc'][0]); ?>" width="100%" height="120" scrolling="no" frameborder="no"></iframe>
+  </div>
+</div>
+<?php
+  }
+?>
