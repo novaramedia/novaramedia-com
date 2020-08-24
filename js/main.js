3db4a9949bce85a914b9f5983dc9c33bb9e5b3d7
varffs <patrick@interglobal.vision>
2020-08-24 20:02:42 +0100

    fix js indentation

--- js/main.js
+++ js/main.js
@@ -291,39 +291,39 @@ Site.Analytics = {
 
     $('.support-form-slider').on('input', _this.debounce(function() {
       dataLayer.push({
-       'event': 'sliderChanged',
-       'target': $(this).data('target'),
-       'amount': this.value,
+        'event': 'sliderChanged',
+        'target': $(this).data('target'),
+        'amount': this.value,
       });
     }, 250));
 
     Site.Header.$menuToggle.click(function() { // bind hamburger click
       dataLayer.push({
-       'event': 'headerToggled'
-       });
+        'event': 'headerToggled'
+      });
     });
 
     $('.related-posts .post').click(function() {
       dataLayer.push({
-       'event': 'relatedPostClicked'
-       });
+        'event': 'relatedPostClicked'
+      });
     });
 
   },
 
   debounce: function(func, wait, immediate) {
-  	var timeout;
-  	return function() {
-  		var context = this, args = arguments;
-  		var later = function() {
-  			timeout = null;
-  			if (!immediate) func.apply(context, args);
-  		};
-  		var callNow = immediate && !timeout;
-  		clearTimeout(timeout);
-  		timeout = setTimeout(later, wait);
-  		if (callNow) func.apply(context, args);
-  	};
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
   },
 };
 
