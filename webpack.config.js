7d7038d2dc500c226b9d25234315c6d4bd30fa6b
varffs <patrickbest@patrickbest.com>
2020-12-31 18:02:05 +0000

    Refactor Webpack
    - dev and build scripts using mode
    - migrate to stylus-native-loader
    - fix output location
    - differnent file moving/copying for fonts
    - remove unused boiler fonts and update fontface

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,20 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyPlugin = require('copy-webpack-plugin');
-const WebpackNotifierPlugin = require('webpack-notifier');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
-module.exports = {
+var config = {
   entry: './src/js/main.js',
 
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
   },
 
   resolve: {
     extensions: ['.js', '.css', '.styl', '.svg']
   },
-
-  mode: 'development',
   
   module: {
     rules: [{
@@ -32,38 +29,54 @@ module.exports = {
           presets: ['@babel/preset-env']
         }
       }
-    }, {
+    },  
+    {
       test: /\.styl/,
       use: [
         {
-          loader: MiniCssExtractPlugin.loader,
-        },
+          loader: MiniCssExtractPlugin.loader, 
+/*
+          options: {
+            publicPath: webpackOptions.output
+          }
+*/
+        }, 
         {
-          loader: 'css-loader',
+          loader: 'css-loader', 
           options: {
             url: false,
+          },
+        }, 
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: ['autoprefixer']
+            }
           }
-        }, {
-          loader: 'stylus-loader'
-        }
+        }, 
+        {
+          loader: 'stylus-native-loader', 
+        },
       ],
-    }, {
-      test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
-      loader: 'file-loader',
-    }],
+    },
+    ],
   },
 
   plugins: [
     new ESLintPlugin(),
     new MiniCssExtractPlugin(),
     new MomentLocalesPlugin(),
-    new WebpackNotifierPlugin({emoji: true, alwaysNotify: true}),
     // Copy the images folder and optimize all the images
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/img/'),
           to: path.resolve(__dirname, 'dist/img/'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/styl/fonts/'),
+          to: path.resolve(__dirname, 'dist/fonts/'),
         },
       ]
     }),
@@ -79,7 +92,13 @@ module.exports = {
   stats: {
     colors: true
   },
+};
 
-  devtool: 'source-map',
-  watch: true,
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map';
+    config.watch = true;
+  }
+  
+  return config;
 };
