5b544967258a449d3030de31ddd1d349ae9def48
varffs <patrickbest@patrickbest.com>
2021-03-05 19:04:20 +0000

    Remove: Stale Kindle widget scripts

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,13 +1,3 @@
 <section id="scripts">
-  <?php
-    wp_footer();
-
-    if (is_single() && in_category('Articles')) {
-  ?>
-    <script type="text/javascript" src="https://d1xnn692s7u6t6.cloudfront.net/widget.js"></script>
-    <script type="text/javascript">(function k(){window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"content":"#single-articles-copy","title":"#single-articles-title","author":"#single-articles-author","published":"#single-articles-publication-date"}):setTimeout(k,500);})();</script>
-  <?php
-    }
-
-  ?>
+  <?php wp_footer(); ?>
 </section>
