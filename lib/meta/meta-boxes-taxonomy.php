09de1bd0473a6694fc90ca3724f7ccc9c84eacee
varffs <patrickbest@patrickbest.com>
2021-06-11 13:25:45 +0100

    add logo and button override options to tax meta

--- lib/meta/meta-boxes-taxonomy.php
+++ lib/meta/meta-boxes-taxonomy.php
@@ -21,9 +21,30 @@ function nm_cmb_taxonomy_metaboxes() {
   ) );
 
   $cmb_term->add_field( array(
+    'name' => esc_html__( 'Microbrand Logo', 'cmb2' ),
+    'desc' => esc_html__( 'if this category is for a microbrand with a logo set it here (optional)', 'cmb2' ),
+    'id'   => $prefix . 'category_logo',
+    'type' => 'file',
+  ) );
+  
+  $cmb_term->add_field( array(
     'name' => esc_html__( 'Podcast subscribe link', 'cmb2' ),
     'desc' => esc_html__( 'url for signup to podcast (optional)', 'cmb2' ),
     'id'   => $prefix . 'podcast_url',
     'type' => 'text_url',
+  ) );
+
+  $cmb_term->add_field( array(
+    'name' => esc_html__( 'Podcast subscribe copy', 'cmb2' ),
+    'desc' => esc_html__( 'override copy for podcast signup links (optional)', 'cmb2' ),
+    'id'   => $prefix . 'podcast_text',
+    'type' => 'text',
+  ) );
+
+  $cmb_term->add_field( array(
+    'name' => esc_html__( 'Youtube subscribe copy', 'cmb2' ),
+    'desc' => esc_html__( 'override copy for Youtube subscribe links (video categories only) (optional)', 'cmb2' ),
+    'id'   => $prefix . 'youtube_text',
+    'type' => 'text',
   ) );
 }
