b466e7071628058636f55ef3f4e41b303a40e919
varffs <patrickbest@patrickbest.com>
2021-09-24 13:30:59 +0100

    update banner copy

--- partials/specials/banners/focus-breaking-britain.php
+++ partials/specials/banners/focus-breaking-britain.php
@@ -6,14 +6,14 @@
     if ($url) {
 ?>
 <div class="padding-top-mid padding-bottom-mid margin-bottom-large background-neon-blue font-color-white" style="overflow: hidden; position: relative">
-  <div style="position: absolute; top: 0; width: 100vw; height: 100vh; overflow: hidden; z-index: 10; pointer-events: none; ">
+  <div style="position: absolute; top: 0; width: 100%; height: 100vh; overflow: hidden; z-index: 10; pointer-events: none; ">
     <div style="position: absolute; top: 0; left: -400px; animation: breakingbars 13131ms ease-in-out 111ms infinite normal forwards;">
       <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-1.svg'); ?>
     </div>
-    <div style="position: absolute; top: -250px; left: 27vw; animation: breakingbars 22222ms ease-in-out 707ms infinite normal forwards;">
+    <div style="position: absolute; top: -250px; left: 27vw; animation: breakingbars 22222ms ease-in-out 77ms infinite normal forwards;">
       <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-2.svg'); ?>
     </div>
-    <div style="position: absolute; top: -50px; right: -250px; animation: breakingbars 11111ms ease-in-out 420ms infinite normal forwards;">
+    <div style="position: absolute; top: -50px; right: -250px; animation: breakingbars 11111ms ease-in-out 42ms infinite normal forwards;">
       <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-3.svg'); ?>
     </div>
   </div>
@@ -57,8 +57,8 @@
         <a href="<?php echo $url; ?>"><h3 class="font-size-neg1">Breaking<br/>Britain.</h3></a>
       </div>
       <div class="flex-grid-item flex-item-s-12 flex-item-xxl-6">  
-        <a href="<?php echo $url; ?>"><p class="font-size-3">Will Britain as we know it survive the 21st Century? Novara ventures in the possibilities of a fragmented future in the new focus featuring __, ____ and ____.</p></a>
-        <a href="<?php echo $url; ?>" class="nm-button nm-button--white nm-button--inline font-color-black">Read the Focus</a>
+        <a href="<?php echo $url; ?>"><p class="font-size-3">With growing rifts between its regions, how much longer can the UK survive? From the economic to the constitutional, this focus explores the divisions and contradictions within the union today.</p></a>
+        <a href="<?php echo $url; ?>" class="nm-button nm-button--white nm-button--inline font-color-black">Read more</a>
       </div>
     </div>
   </div>
