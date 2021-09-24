e01bb5c295747cb83d90cfbd900fe3d307aeb99e
varffs <patrickbest@patrickbest.com>
2021-09-24 13:31:14 +0100

    update layout for focus archive header

--- specials/taxonomy-focus-breaking-britain.php
+++ specials/taxonomy-focus-breaking-britain.php
@@ -7,14 +7,14 @@ $quotes = get_term_meta($term->term_id, '_nm_focus_quotes', true);
 $credits = get_term_meta($term->term_id, '_nm_focus_credits', true);
 ?>
 
-<div style="position: absolute; width: 100vw; height: 100vh; overflow: hidden; z-index: 10; pointer-events: none; ">
+<div style="position: absolute; width: 100%; height: 100vh; overflow: hidden; z-index: 10; pointer-events: none; ">
   <div style="position: absolute; top: 0; left: -400px; animation: breakingbars 13131ms ease-in-out 111ms infinite normal forwards;">
     <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-1.svg'); ?>
   </div>
-  <div style="position: absolute; top: -250px; left: 27vw; animation: breakingbars 22222ms ease-in-out 707ms infinite normal forwards;">
+  <div style="position: absolute; top: -250px; left: 27vw; animation: breakingbars 22222ms ease-in-out 77ms infinite normal forwards;">
     <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-2.svg'); ?>
   </div>
-  <div style="position: absolute; top: -50px; right: -250px; animation: breakingbars 11111ms ease-in-out 420ms infinite normal forwards;">
+  <div style="position: absolute; top: -50px; right: -250px; animation: breakingbars 11111ms ease-in-out 42ms infinite normal forwards;">
     <?php echo url_get_contents(get_bloginfo('stylesheet_directory') . '/dist/img/specials/focus-breakingbritain-line-3.svg'); ?>
   </div>
 </div>
@@ -53,14 +53,16 @@ $credits = get_term_meta($term->term_id, '_nm_focus_credits', true);
 
   <!-- main posts loop -->
   <section id="posts" class="container">    
-    <div class="flex-grid-row margin-top-small margin-bottom-mid">
-      <div class="flex-grid-item flex-item-s-12 flex-item-l-12 flex-item-xxl-12">
+    <div class="flex-grid-row margin-top-small margin-bottom-mid" style="align-items: flex-end;">
+      <div class="flex-grid-item flex-item-s-12 flex-item-m-12 flex-item-xxl-7">
         <h4 class="margin-bottom-micro">Focus</h4>
-        <h1 class="margin-bottom-micro font-color-blue-neon" style="font-size: 239px;font-weight: 700;line-height: 203px;letter-spacing: -0.04em;">Breaking Britain.</h1>
-        <div class="font-size-h2">
-          <?php echo category_description(); ?>
-        </div>
+        <h1 class="margin-bottom-micro font-color-blue-neon" style="font-size: 239px;font-weight: 700;line-height: 203px;letter-spacing: -0.04em;margin-bottom: 0;margin-left: -0.06em;">Breaking Britain.</h1>
       </div>        
+      <div class="flex-grid-item flex-item-s-12 flex-item-m-9 flex-item-xxl-5">
+        <div class="font-size-h2">
+          <?php echo get_term_field( 'description', $term, null, $context = 'raw' ); // getting like this to avoid the filters that add <p> tags ?>
+        </div>
+      </div>
     </div>
 
     <div class="flex-grid-row margin-bottom-basic">
