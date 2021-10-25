2cdff3c1fd85493c5a527f163b4ac19b27a79940
varffs <patrickbest@patrickbest.com>
2021-10-25 19:11:41 +0100

    Fix: Url encoded non url

--- single-job.php
+++ single-job.php
@@ -11,8 +11,8 @@ if( have_posts() ) {
   while( have_posts() ) {
     the_post();
     $meta = get_post_meta($post->ID);
-    
-    $email_subject = urlencode(strtoupper($post->post_title));
+
+    $email_subject = strtoupper($post->post_title);
 ?>
   <!-- main posts loop -->
   <article id="job" class="container margin-top-small margin-bottom-large">
@@ -24,9 +24,9 @@ if( have_posts() ) {
     <div class="row margin-bottom-basic">
       <div class="col col10 page-copy">
         <?php the_content(); ?>
-        <p>Send your application in a single email to <a href="mailto:info@novaramedia.com?subject=<?php echo $email_subject; ?>">info@novaramedia.com</a> with <?php echo $email_subject; ?> in the subject line.</p>
+        <p>Send your application in a single email to <a href="mailto:info@novaramedia.com?subject=<?php echo urlencode($email_subject); ?>">info@novaramedia.com</a> with <?php echo $email_subject; ?> in the subject line.</p>
         <p>Closing date: <?php echo date('j F Y', $meta['_nm_deadline'][0]); ?></p>
-      </div>      
+      </div>
     </div>
   <!-- end post -->
   </article>
