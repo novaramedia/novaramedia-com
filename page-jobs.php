979b3b1b2fd5813a82ca1e692defc4fd447865a0
varffs <patrickbest@patrickbest.com>
2021-10-25 17:13:54 +0100

    Detail: Jobs listing styling
    make list of links bollder

--- page-jobs.php
+++ page-jobs.php
@@ -15,7 +15,7 @@ if( have_posts() ) {
         <h4><?php the_title(); ?></h4>
       </div>
     </div>
-    
+
     <div class="flex-grid-row flex-grid-row-m--reverse margin-bottom-small">
       <div class="flex-grid-item flex-item-m-12 flex-item-l-8 flex-item-xl-7 flex-item-xxl-6">
         <?php the_content(); ?>
@@ -30,13 +30,13 @@ if( have_posts() ) {
             	'meta_key' => '_nm_deadline',
             	'meta_value' => (date('U') - 86400), // today minus one day of seconds to make display inclusive of deadline day
             	'meta_type' => 'NUMERIC',
-            	'meta_compare' => '>=',	
+            	'meta_compare' => '>=',
           ));
-          
+
           if (!empty($jobs)) {
         ?>
-        <h5>We are currently hiring:</h5>
-        <ul>
+        <h5 class="font-size-2">We are currently hiring:</h5>
+        <ul class="list--links font-size-2">
         <?php
             foreach ($jobs as $job) {
               $deadline = get_post_meta($job->ID, '_nm_deadline', true);
@@ -46,7 +46,7 @@ if( have_posts() ) {
             }
           } else {
         ?>
-        <h5>There are currently no available positions</h5>
+        <h5 class="mobile-padding-bottom-small">There are currently no available positions</h5>
         <?php
           }
         ?>
