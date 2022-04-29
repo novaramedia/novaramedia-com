ab5c3a597c86db7d586d3de8994c9cc34d2daf5b
varffs <patrickbest@patrickbest.com>
2022-04-29 23:44:57 +0100

    toggle autovalues for oneoff or regular

--- partials/support-section.php
+++ partials/support-section.php
@@ -1,27 +1,27 @@
 <?php
   $override_text = isset($args['override_text']) ? $args['override_text'] : false;
-    
+
   $heading_copy = !empty($args['heading_copy']) ? $args['heading_copy'] : 'Support Us';
-  
+
   $support_section_text = IGV_get_option('_igv_support_section_text');
-  
+
   $fundraiser_expiration = IGV_get_option('_igv_fundraiser_end_time');
   $fundraiser_form_text = IGV_get_option('_igv_fundraiser_form_text');
 
-  $default_subscription_value = 8;
-  
+  $support_section_autovalues = SUPPORT_SECTION_AUTOVALUES;
+
   $instance = uniqid('support-form-');
 ?>
 
 <div id="<?php echo $instance; ?>" class="support-section background-red font-color-white padding-top-large padding-bottom-large mobile-padding-top-basic mobile-padding-bottom-basic">
-  <div class="container">    
-    
-    <form 
-      class="support-form" 
-      action="https://donate.novaramedia.com/regular" 
+  <div class="container">
+
+    <form
+      class="support-form"
+      action="https://donate.novaramedia.com/regular"
     >
-      <input class="support-form__value-input" type="hidden" value="<?php echo $default_subscription_value; ?>" name="amount" />
-            
+      <input class="support-form__value-input" type="hidden" value="<?php echo $support_section_autovalues['default'][0]; ?>" name="amount" />
+
       <div class="flex-grid-row margin-bottom-small">
         <div class="flex-grid-item flex-item-m-12">
           <a href="<?php echo home_url('support/'); ?>">
@@ -31,17 +31,17 @@
       </div>
 
       <div class="flex-grid-row font-size-2 font-size-s-1">
-        <div class="flex-grid-item flex-item-m-12 flex-item-l-8 flex-item-xl-6 flex-item-xxl-5">      
+        <div class="flex-grid-item flex-item-m-12 flex-item-l-8 flex-item-xl-6 flex-item-xxl-5">
           <?php
             if ($support_section_text || $override_text) {
             ?>
           <div class="margin-top-micro margin-bottom-small font-bold">
-            <a href="<?php echo home_url('support/'); ?>" class="js-fix-widows"><?php 
+            <a href="<?php echo home_url('support/'); ?>" class="js-fix-widows"><?php
               if ($override_text) {
                 echo $override_text;
               } else {
                 echo $support_section_text;
-              } 
+              }
             ?></a>
           </div>
             <?php
@@ -52,13 +52,13 @@
         <div class="flex-grid-item flex-item-l-12 flex-offset-xl-0 flex-item-xxl-6 flex-offset-xxl-1">
           <div class="flex-grid-row flex-grid--nested-tight margin-bottom-tiny">
             <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-2">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="8">£8</button>
+              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default'][0]; ?>">£<?php echo $support_section_autovalues['default'][0]; ?></button>
             </div>
             <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-2">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="10">£10</button>
+              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default'][1]; ?>">£<?php echo $support_section_autovalues['default'][1]; ?></button>
             </div>
             <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-2">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="20">£20</button>
+              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default'][2]; ?>">£<?php echo $support_section_autovalues['default'][2]; ?></button>
             </div>
             <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-6">
               <label for="<?php echo $instance; ?>__custom-input" class="u-visuallyhidden">Custom donation amount in pounds</label>
@@ -66,11 +66,11 @@
             </div>
           </div>
           <div class="flex-grid-row flex-grid--nested-tight">
-            <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-3">          
-              <button class="support-form__button support-form__schedule-option" data-action="set-type" data-value="https://donate.novaramedia.com/oneoff">One-off</button>
+            <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-3">
+              <button class="support-form__button support-form__schedule-option" data-action="set-type" data-value="oneoff">One-off</button>
             </div>
-            <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-3">          
-              <button class="support-form__button support-form__button--active support-form__schedule-option" data-action="set-type" data-value="https://donate.novaramedia.com/regular">Monthly</button>
+            <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-3">
+              <button class="support-form__button support-form__button--active support-form__schedule-option" data-action="set-type" data-value="regular">Monthly</button>
             </div>
             <div class="flex-grid-item flex-grid-item--tight flex-item-xxl-6">
               <input class="support-form__submit nm-button nm-button--red-dark" type="submit" value="Go" />
@@ -78,6 +78,6 @@
           </div>
         </div>
       </div>
-    </form>      
+    </form>
   </div>
 </div>
--- src/js/modules/Support.js
+++ src/js/modules/Support.js
@@ -1,17 +1,29 @@
 /* jshint esversion: 6, browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
+/* global WP */
 
 import $ from 'jquery';
 
 export class Support {
+  constructor() {
+    this.donationAppUrl = 'https://donate.novaramedia.com/';
+  }
+
   onReady() {
     const _this = this;
 
     if ($('.support-section').length) {
+      _this.setupAutovalues();
       _this.bind();
     }
   }
 
+  setupAutovalues() {
+    this.autovalues = WP.supportSectionAutovalues['default'];
+  }
+
   bind() {
+    const _this = this;
+
     $('.support-form').each(function (index, value) {
       const $form = $(value);
 
@@ -24,20 +36,23 @@ export class Support {
           const $button = $(this);
           const data = $button.data();
 
-          if (data.action === 'set-type') {
-            $form.attr('action', data.value);
+          if (data.action === 'set-type') { // if the button is setting the type of donation
+            _this.setAutoValues($form, data.value);
+
+            $form.attr('action', _this.donationAppUrl + data.value);
 
             $form
               .find('.support-form__button[data-action="set-type"]')
               .removeClass('support-form__button--active');
 
             $button.addClass('support-form__button--active');
-          } else if (data.action === 'set-value') {
+          } else if (data.action === 'set-value') { // if the button is setting the donation value
             $valueInput.val(data.value);
 
             $form
               .find('.support-form__button[data-action="set-value"]')
               .removeClass('support-form__button--active');
+
             $('.support-form__custom-input').removeClass(
               'support-form__button--active'
             );
@@ -60,6 +75,22 @@ export class Support {
           $(this).addClass('support-form__button--active');
         },
       });
+    });
+  }
+
+  /**
+   * Switches the quick auto values displayed on the form depending on the type of donation.
+   *
+   * @param {Object}   $form         jQuery object of the form in question.
+   * @param {String}   donationType  Type of donation. oneoff OR regular
+   */
+  setAutoValues($form, donationType) {
+    const _this = this;
+
+    $form.find('.support-form__value-option').each((index, input) => {
+      const autovaluesIndex = donationType === 'oneoff' ? index + 3 : index;
+      const value = _this.autovalues[autovaluesIndex];
+      $(input).data('value', value).text(`£${value}`);
     });
   }
 
