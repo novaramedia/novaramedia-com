fef2f101eae0a25468e7da80fa9e49c07273b824
varffs <patrickbest@patrickbest.com>
2022-05-20 12:02:19 +0100

    show fundraising banners at top of cat archives

--- category.php
+++ category.php
@@ -1,6 +1,8 @@
 <?php
 get_header();
 
+$show_support_banner = NM_get_option('nm_front_page_settings_banners_show_support');
+
 $video = get_category_by_slug('video');
 $category = get_category(get_query_var('cat'));
 
@@ -23,6 +25,13 @@ if ($category->slug === 'video') {
 }
 ?>
 <main id="main-content" class="category-archive">
+<?php if ($show_support_banner !== false) {
+  if ($category->slug === 'video') {
+    get_template_part('partials/specials/banners/support-video');
+  } else {
+    get_template_part('partials/front-page/front-page-support-banner');
+  }
+} ?>
   <section id="posts" class="container margin-top-small">
     <div class="flex-grid-row margin-bottom-basic">
       <?php
@@ -79,63 +88,65 @@ if ($category->slug === 'video') {
         }
       ?>
     </div>
+  </section>
 <?php
   if ($is_video) {
-
-    if (have_posts()) {
-      $i = 0;
-      the_post();
-    ?>
-    <div class="row margin-bottom-large only-desktop">
-      <div class="col col16">
-        <?php
-        $meta = get_post_meta($post->ID);
-        if (!empty($meta['_cmb_utube'])) {
-        ?>
-        <div class="u-video-embed-container">
-          <iframe class="youtube-player" type="text/html" src="<?php echo generate_youtube_embed_url($meta['_cmb_utube'][0]); ?>"></iframe>
+      if (have_posts()) {
+        $i = 0;
+        the_post();
+      ?>
+    <div class="container">
+      <div class="row margin-bottom-large only-desktop">
+        <div class="col col16">
+          <?php
+          $meta = get_post_meta($post->ID);
+          if (!empty($meta['_cmb_utube'])) {
+          ?>
+          <div class="u-video-embed-container">
+            <iframe class="youtube-player" type="text/html" src="<?php echo generate_youtube_embed_url($meta['_cmb_utube'][0]); ?>"></iframe>
+          </div>
+          <a href="<?php the_permalink(); ?>">
+            <h6 class="js-fix-widows margin-top-micro"><?php the_title(); ?></h6>
+          </a>
+          <?php
+          } else {
+            echo 'Someone messed up :/';
+          }
+          ?>
         </div>
-        <a href="<?php the_permalink(); ?>">
-          <h6 class="js-fix-widows margin-top-micro"><?php the_title(); ?></h6>
-        </a>
-        <?php
-        } else {
-          echo 'Someone messed up :/';
-        }
-        ?>
-      </div>
-      <div class="col col4">
-        <?php
-        if (have_posts()) {
-          while(have_posts() && $i < 6) {
-            the_post();
-        ?>
-        <a href="<?php the_permalink(); ?>">
-         <div class="single-tv-related-tv margin-bottom-small">
-           <?php the_post_thumbnail('col4-16to9'); ?>
-           <h6 class="js-fix-widows margin-top-micro"><?php the_title(); ?></h6>
-         </div>
-       </a>
-        <?php
-          if ($i === 2) {
-            echo '</div><div class="col col4">';
-          }
+        <div class="col col4">
+          <?php
+          if (have_posts()) {
+            while(have_posts() && $i < 6) {
+              the_post();
+          ?>
+          <a href="<?php the_permalink(); ?>">
+           <div class="single-tv-related-tv margin-bottom-small">
+             <?php the_post_thumbnail('col4-16to9'); ?>
+             <h6 class="js-fix-widows margin-top-micro"><?php the_title(); ?></h6>
+           </div>
+         </a>
+          <?php
+            if ($i === 2) {
+              echo '</div><div class="col col4">';
+            }
 
-          $i++;
+            $i++;
+            }
           }
-        }
-        ?>
+          ?>
+        </div>
       </div>
     </div>
-  <?php
-    }
+    <?php
+      }
 
-    // reset pointer for have_posts
-    global $wp_query;
-    $wp_query->current_post = -1;
+      // reset pointer for have_posts
+      global $wp_query;
+      $wp_query->current_post = -1;
   }
 ?>
-
+  <div class="container">
     <div class="flex-grid-row margin-bottom-basic">
 <?php
 if( have_posts() ) {
@@ -158,7 +169,7 @@ if( have_posts() ) {
         <?php get_template_part('partials/pagination'); ?>
       </div>
     </div>
-  </section>
+  </div>
 </main>
 <?php
 get_footer();
