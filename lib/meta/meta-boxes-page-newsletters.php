c7d09177909fe8062cee47e53b951b74eb9ff4cf
varffs <patrickbest@patrickbest.com>
2022-10-14 14:00:04 +0100

    Alt page template
    Designed around use for behind the scenes content

--- lib/meta/meta-boxes-page-newsletters.php
+++ lib/meta/meta-boxes-page-newsletters.php
@@ -1,31 +1,39 @@
 <?php
 
-add_action( 'cmb2_init', 'nm_cmb_page_newsletters_metaboxes' );
+add_action( 'cmb2_init', 'nm_cmb_page_text_copy_template' );
 
-function nm_cmb_page_newsletters_metaboxes() {
+function nm_cmb_page_text_copy_template() {
   $prefix = '_nm_';
 
   $cmb_term = new_cmb2_box( array(
-    'id'         => $prefix . 'page_newsletters',
+    'id'         => $prefix . 'page_text-copy',
     'title'      => esc_html__( 'Newsletters Metabox', 'cmb2' ),
     'object_types' => array( 'page' ), // Post type
-    'show_on'    => array('key' => 'slug', array('the-pick', 'the-cortado')),
+    'show_on'    => array('key' => 'page-template', 'value' => 'page__text-copy.php'),
     'context'    => 'normal',
     'priority'   => 'high',
     'show_names' => true, // Show field names on the left
   ) );
 
   $cmb_term->add_field( array(
-    'name' => esc_html__( 'Youtube embed code', 'cmb2' ),
-    'desc' => esc_html__( 'Id of Youtube video. for example if this is the url https://www.youtube.com/watch?v=CmuDcXfBqTg&feature=c4-overview&list=UUOzMAa6IhV6uwYQATYG_2kg then the Id is the value after the ?v= and before the &, for this link CmuDcXfBqTg. Overrides featured image with Youtube embed for promo video (optional)', 'cmb2' ),
-    'id'   => $prefix . 'youtube_id',
+    'name' => esc_html__( 'Page subtitle', 'cmb2' ),
+    'desc' => esc_html__( '... (optional)', 'cmb2' ),
+    'id'   => $prefix . 'subtitle',
     'type' => 'text',
   ) );
 
   $cmb_term->add_field( array(
-    'name' => esc_html__( 'Support section override text', 'cmb2' ),
-    'desc' => esc_html__( 'override copy for the support section below the email signup (optional)', 'cmb2' ),
-    'id'   => $prefix . 'support_text',
-    'type' => 'textarea_small',
+    'name' => esc_html__( 'CTA link text', 'cmb2' ),
+    'desc' => esc_html__( '... (optional)', 'cmb2' ),
+    'id'   => $prefix . 'cta-copy',
+    'type' => 'text',
+  ) );
+
+  $cmb_term->add_field( array(
+    'name' => esc_html__( 'CTA link', 'cmb2' ),
+    'desc' => esc_html__( '... (optional)', 'cmb2' ),
+    'id'   => $prefix . 'cta-link',
+    'type' => 'text_url',
   ) );
 }
+
---
+++ lib/meta/meta-boxes-text-copy-page-template.php
@@ -0,0 +1,31 @@
+<?php
+
+add_action( 'cmb2_init', 'nm_cmb_page_newsletters_metaboxes' );
+
+function nm_cmb_page_newsletters_metaboxes() {
+  $prefix = '_nm_';
+
+  $cmb_term = new_cmb2_box( array(
+    'id'         => $prefix . 'page_newsletters',
+    'title'      => esc_html__( 'Newsletters Metabox', 'cmb2' ),
+    'object_types' => array( 'page' ), // Post type
+    'show_on'    => array('key' => 'slug', array('the-pick', 'the-cortado')),
+    'context'    => 'normal',
+    'priority'   => 'high',
+    'show_names' => true, // Show field names on the left
+  ) );
+
+  $cmb_term->add_field( array(
+    'name' => esc_html__( 'Youtube embed code', 'cmb2' ),
+    'desc' => esc_html__( 'Id of Youtube video. for example if this is the url https://www.youtube.com/watch?v=CmuDcXfBqTg&feature=c4-overview&list=UUOzMAa6IhV6uwYQATYG_2kg then the Id is the value after the ?v= and before the &, for this link CmuDcXfBqTg. Overrides featured image with Youtube embed for promo video (optional)', 'cmb2' ),
+    'id'   => $prefix . 'youtube_id',
+    'type' => 'text',
+  ) );
+
+  $cmb_term->add_field( array(
+    'name' => esc_html__( 'Support section override text', 'cmb2' ),
+    'desc' => esc_html__( 'override copy for the support section below the email signup (optional)', 'cmb2' ),
+    'id'   => $prefix . 'support_text',
+    'type' => 'textarea_small',
+  ) );
+}
