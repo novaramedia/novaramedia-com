c4434070866ed68188b1c1415c4af58e360990c7
varffs <patrickbest@patrickbest.com>
2023-06-09 12:16:49 +0100

    scaffold a bottom bar & refactor gdpr
    and refactor gdpr component

---
+++ partials/bottom-bar/bar-support.php
@@ -0,0 +1,3 @@
+<div id="bar-support">
+
+</div>
---
+++ partials/bottom-bar/bottom-bar.php
@@ -0,0 +1,6 @@
+<div id="bottom-bar">
+  <?php
+    get_template_part('partials/bottom-bar/bar-support');
+    get_template_part('partials/bottom-bar/gdpr');
+  ?>
+</div>
