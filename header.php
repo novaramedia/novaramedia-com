480dcfa36c839b5c51cee29790b5113963563c86
varffs <patrickbest@patrickbest.com>
2023-10-06 15:51:47 +0100

    Add condensed Roboto
    possibly temp

--- header.php
+++ header.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="dns-prefetch" href="https://googletagmanager.com"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
   <?php
     get_template_part('partials/header/google-tag-manager');
     get_template_part('partials/header/seo');
