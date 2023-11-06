64ba8029505cab6c43e38b3e4df1b71563271460
varffs <patrickbest@patrickbest.com>
2023-11-06 11:48:21 +0000

    Refactor: Support video banner

--- partials/specials/banners/support-video.php
+++ partials/specials/banners/support-video.php
@@ -5,29 +5,29 @@
   if (!empty($youtube_id) && !empty($copy)) {
 ?>
 <div class="background-red background-support-texture">
-  <div class="container padding-top-basic padding-bottom-basic font-color-white">
-    <div class="flex-grid-row margin-bottom-small">
-      <div class="flex-grid-item flex-item-s-12">
+  <div class="container pt-5 pb-5 font-color-white">
+    <div class="flex-grid-row mb-3">
+      <div class="flex-grid-item flex-item-s-12 fs-3-sans font-uppercase font-weight-bold">
         <a href="<?php echo home_url('support/'); ?>"><h4>Support Us</h4></a>
       </div>
     </div>
     <div class="flex-grid-row">
-      <div class="flex-grid-item flex-item-s-12 flex-offset-m-1 flex-item-m-10 flex-item-xl-6 flex-item-xxl-7 mobile-margin-bottom-small">
+      <div class="flex-grid-item flex-offset-s-0 flex-item-s-12 flex-offset-m-1 flex-item-m-10 flex-item-xl-6 flex-item-xxl-7 mb-s-3">
         <div class="u-video-embed-container background-red">
           <iframe class="youtube-player lazyload" data-src="<?php echo generate_youtube_embed_url($youtube_id); ?>" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
-      <div class="flex-grid-item flex-item-s-12 flex-offset-m-1 flex-item-m-10 flex-item-xl-6 flex-item-xxl-4">
+      <div class="flex-grid-item flex-offset-s-0 flex-item-s-12 flex-offset-m-1 flex-item-m-10 flex-item-xl-6 flex-item-xxl-4">
         <a href="<?php echo site_url('support/'); ?>">
           <?php
             foreach($copy as $key => $paragraph) {
               if ($key === 0) {
             ?>
-            <h3 class="font-size-3 margin-bottom-tiny js-fix-widows"><?php echo $paragraph; ?></h3>
+            <h3 class="fs-7 mb-3 js-fix-widows"><?php echo $paragraph; ?></h3>
             <?php
               } else {
             ?>
-            <p class="font-size-2"><?php echo $paragraph; ?></p>
+            <p class="fs-5-sans mb-3"><?php echo $paragraph; ?></p>
             <?php
               }
           ?>
