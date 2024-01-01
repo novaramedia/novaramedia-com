245917dabeb72373aff5f9887a5de7d4349ad9c7
varffs <patrickbest@patrickbest.com>
2024-01-01 21:20:39 +0000

    tags and search archive

--- index.php
+++ index.php
@@ -1,63 +1,54 @@
 <?php
 get_header();
 ?>
-
-<!-- main content -->
-
 <main id="main-content">
-
-  <!-- main posts loop -->
-  <section id="posts" class="container margin-top-small">
-
+  <section id="posts" class="container mt-3">
 <?php
-if (is_search()) {
+if (is_search() || is_tag()) {
 ?>
-    <div class="flex-grid-row margin-bottom-basic">
-      <div class="flex-grid-item flex-item-s-12">
-        <h4>Search Results for: <?php echo get_search_query(); ?></h4>
-      </div>
-    </div>
+    <div class="grid-row mb-5">
+      <div class="grid-item is-xxl-24">
+        <h4 class="fs-4-sans">
 <?php
-} else if (is_tag()) {
+  if (is_search()) {
 ?>
-    <div class="flex-grid-row margin-bottom-basic">
-      <div class="flex-grid-item flex-item-s-12">
-        <h4><?php single_tag_title('Tag: '); ?></h4>
+          Search Results for: <?php echo get_search_query(); ?></h4>
+<?php
+  } else if (is_tag()) {
+?>
+          Tag: <?php single_tag_title(); ?></h4>
+<?php
+  }
+?>
       </div>
     </div>
 <?php
 }
 ?>
-    <div class="flex-grid-row margin-bottom-basic">
+    <div class="grid-row mb-5">
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
 
     get_template_part('partials/post-layouts/flex-post', null, array(
-      'grid-item-classes' => 'flex-grid-item flex-item-s-12 flex-item-l-6 flex-item-xxl-4 margin-bottom-basic',
+      'grid-item-classes' => 'grid-item is-s-24 is-l-12 is-xxl-8 mb-4',
       'image-size' => 'col12-16to9',
     ));
   }
 } else {
 ?>
-    <article class="flex-grid-item flex-item-s-12"><?php _e('Sorry, nothing matched your criteria :/'); ?></article>
+    <article class="grid-item is-s-24"><?php _e('Sorry, nothing matched your criteria :/'); ?></article>
 <?php
 } ?>
     </div>
-    <div class="flex-grid-row margin-bottom-basic">
-      <div class="flex-grid-item flex-item-s-12">
+    <div class="grid-row mb-5">
+      <div class="grid-item is-xxl-24">
         <?php get_template_part('partials/pagination'); ?>
       </div>
     </div>
-
-  <!-- end posts -->
   </section>
-
-<!-- end main-content -->
-
 </main>
-
 <?php
 get_footer();
 ?>
