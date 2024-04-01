a38f0e040ad1ed3b9e75fa98828272efe6d6ef9d
varffs <patrickbest@patrickbest.com>
2024-04-01 15:53:30 +0100

    scaffold states

--- partials/email-signup.php
+++ partials/email-signup.php
@@ -64,22 +64,24 @@
         <form class="email-signup__form" action="<?php echo $netlify; ?>" method="post" target="_blank">
           <input type="hidden" name="newsletter" value="<?php echo $mailchimp_key; ?>" />
 
-          <div class="form-group mb-2 fs-6">
-            <label class="u-visuallyhidden" for="firstName">First name:</label>
-            <input name="firstName" class="email-signup__name-input" id="firstName" type="text" autocomplete="given-name" placeholder="First name" />
-          </div>
+          <div class="email-signup__inputs">
+            <div class="form-group mb-2 fs-6">
+              <label class="u-visuallyhidden" for="firstName">First name:</label>
+              <input name="firstName" class="email-signup__name-input" id="firstName" type="text" autocomplete="given-name" placeholder="First name" />
+            </div>
 
-          <div class="form-group mb-2 fs-6">
-            <label class="u-visuallyhidden" for="email">Email:</label>
-            <input name="email" class="email-signup__email-input" id="email" type="email" autocomplete="email" placeholder="Email" required />
-          </div>
+            <div class="form-group mb-2 fs-6">
+              <label class="u-visuallyhidden" for="email">Email:</label>
+              <input name="email" class="email-signup__email-input" id="email" type="email" autocomplete="email" placeholder="Email" required />
+            </div>
 
-          <div class="email-signup__email-gdpr-group form-group mb-2">
-            <label for="newsletter-gdpr" class="fs-2">I agree to the <a target="_blank" rel="noopener" href="<?php echo site_url('privacy-policy/'); ?>">Privacy Policy</a></label>
-            <input name="gdpr" class="email-signup__email-gdpr-input" id="newsletter-gdpr" type="checkbox" value="accepted" required/>
-          </div>
+            <div class="email-signup__email-gdpr-group form-group mb-2">
+              <label for="newsletter-gdpr" class="fs-2">I agree to the <a target="_blank" rel="noopener" href="<?php echo site_url('privacy-policy/'); ?>">Privacy Policy</a></label>
+              <input name="gdpr" class="email-signup__email-gdpr-input" id="newsletter-gdpr" type="checkbox" value="accepted" required/>
+            </div>
 
-          <input class="email-signup__submit nm-button nm-button--<?php echo $button_color; ?> fs-6" type="submit" value="Sign up">
+            <input class="email-signup__submit nm-button nm-button--<?php echo $button_color; ?> fs-6" type="submit" value="Sign up">
+          </div>
 
           <div class="email-signup__feedback-processing">
             <div class="spinner">
@@ -88,13 +90,12 @@
             </div>
           </div>
 
-          <div class="flex-grid-row">
-            <div class="flex-grid-item flex-item-s-12 margin-bottom-small email-signup__feedback-failed">
-              <p>Sign up error: <span class="email-signup__feedback-message"></span>. Maybe try again</p>
-            </div>
-            <div class="flex-grid-item flex-item-s-12 margin-bottom-small email-signup__feedback-completed">
-              <p>Thanks for signing up. &#10003;</p>
-            </div>
+          <div class="email-signup__feedback-failed mt-2">
+            <p>Sign up error: <span class="email-signup__feedback-message"></span>. Maybe try again</p>
+          </div>
+
+          <div class="email-signup__feedback-completed fs-6 fs-s-4-sans">
+            <p>Thanks for signing up. &#10003;</p>
           </div>
         </form>
       </div>
