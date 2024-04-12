00bc5fe5264d22b9b9e57e6b532eb75a6beb94ce
varffs <patrickbest@patrickbest.com>
2024-04-12 18:02:03 +0100

    update support section form

--- partials/support-section.php
+++ partials/support-section.php
@@ -46,31 +46,31 @@
         <div class="support-form__inputs grid-item is-l-24 offset-xl-0 is-xxl-12 offset-xxl-2 fs-s-3-sans">
           <div class="grid-row grid--nested-tight margin-bottom-tiny">
             <div class="grid-item grid-item--tight is-xxl-4">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_low ?>" data-name="low"
+              <button class="support-form__button support-form__value-option ui-input" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_low ?>" data-name="low"
               >£<?php echo $support_section_autovalues['default']->regular_low ?></button>
             </div>
             <div class="grid-item grid-item--tight is-xxl-4">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_medium ?>" data-name="medium"
+              <button class="support-form__button support-form__value-option ui-input" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_medium ?>" data-name="medium"
               >£<?php echo $support_section_autovalues['default']->regular_medium ?></button>
             </div>
             <div class="grid-item grid-item--tight is-xxl-4">
-              <button class="support-form__button support-form__value-option" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_high ?>" data-name="high"
+              <button class="support-form__button support-form__value-option ui-input" data-action="set-value" data-value="<?php echo $support_section_autovalues['default']->regular_high ?>" data-name="high"
               >£<?php echo $support_section_autovalues['default']->regular_high ?></button>
             </div>
             <div class="grid-item grid-item--tight is-xxl-12">
               <label for="<?php echo $instance; ?>__custom-input" class="u-visuallyhidden">Custom donation amount in pounds</label>
-              <input id="<?php echo $instance; ?>__custom-input" class="support-form__custom-input" type="number" placeholder="£ Custom amount" />
+              <input id="<?php echo $instance; ?>__custom-input" class="support-form__custom-input ui-input" type="number" min="1" placeholder="£ Custom amount" />
             </div>
           </div>
           <div class="grid-row grid--nested-tight">
             <div class="grid-item grid-item--tight is-xxl-6">
-              <button class="support-form__button support-form__schedule-option" data-action="set-type" data-value="oneoff">One-off</button>
+              <button class="support-form__button support-form__schedule-option ui-input" data-action="set-type" data-value="oneoff">One-off</button>
             </div>
             <div class="grid-item grid-item--tight is-xxl-6">
-              <button class="support-form__button support-form__button--active support-form__schedule-option" data-action="set-type" data-value="regular">Monthly</button>
+              <button class="support-form__button support-form__button--active support-form__schedule-option ui-input" data-action="set-type" data-value="regular">Monthly</button>
             </div>
             <div class="grid-item grid-item--tight is-xxl-12">
-              <input class="support-form__submit nm-button nm-button--red-dark" type="submit" value="Go" />
+              <input class="support-form__submit ui-button ui-button--white ui-button--fill-width" type="submit" value="Go" />
             </div>
           </div>
         </div>
