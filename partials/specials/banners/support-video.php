880d7f5ab9cb013cdc1c09a2b283cd1ae7e436d4
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2024-11-29 10:18:01 +0000

    linting corrections

--- partials/specials/banners/support-video.php
+++ partials/specials/banners/support-video.php
@@ -1,5 +1,4 @@
 <?php
-// Get options and values
 $default_support_section_text = NM_get_option('nm_fundraising_settings_support_section_text', 'nm_fundraising_options');
 $default_support_section_heading = !empty($args['heading_copy']) ? $args['heading_copy'] : NM_get_option('nm_fundraising_settings_support_section_title', 'nm_fundraising_options');
 $custom_youtube_id = NM_get_option('nm_fundraising_settings_video_banner_cta_youtube_id', 'nm_fundraising_options');
@@ -10,7 +9,7 @@ $support_page = get_page_by_path("support");
 $suport_page_youtube_id = false;
 
 if (!$support_page === null) {
-  $suport_page_youtube_id = get_post_meta($support_page->ID, '_nm_support_youtube', true);
+    $suport_page_youtube_id = get_post_meta($support_page->ID, '_nm_support_youtube', true);
 }
 
 ?>
@@ -22,14 +21,14 @@ if (!$support_page === null) {
       <div class="grid-item is-m-24 is-l-16 is-xl-12 is-xxl-10">
         <div class="u-video-embed-container background-red">
           <iframe class="youtube-player lazyload" data-src="<?php
-                                                            if ($custom_youtube_id) {
-                                                              echo generate_youtube_embed_url($custom_youtube_id);
-                                                            } else if ($support_page_youtube_id) {
-                                                              echo generate_youtube_embed_url($support_page_youtube_id);
-                                                            } else {
-                                                              echo generate_youtube_embed_url("c6hfjBmzt5c");
-                                                            }
-                                                            ?>" frameborder="0" allowfullscreen></iframe>
+            if ($custom_youtube_id) {
+                echo generate_youtube_embed_url($custom_youtube_id);
+            } else if ($support_page_youtube_id) {
+                echo generate_youtube_embed_url($support_page_youtube_id);
+            } else {
+                echo generate_youtube_embed_url("c6hfjBmzt5c");
+            }
+      ?>" frameborder="0" allowfullscreen></iframe>
         </div>
       </div>
       <div class="support-form__inputs  grid-item is-l-24 offset-xl-0 is-xxl-12 offset-xxl-2">
@@ -40,25 +39,25 @@ if (!$support_page === null) {
           <div class="margin-top-micro margin-bottom-small">
             <h3 class="font-size-13 font-weight-bold mb-4 mt-4 js-fix-widows">
               <?php
-              if ($custom_headline) {
-                echo $custom_headline;
-              } else if ($default_support_section_heading) {
-                echo $default_support_section_heading;
-              } else {
-                echo "Support Us";
-              }
-              ?>
+                if ($custom_headline) {
+                    echo $custom_headline;
+                } else if ($default_support_section_heading) {
+                    echo $default_support_section_heading;
+                } else {
+                    echo "Support Us";
+                }
+                ?>
             </h3>
           </div>
           <div class="margin-top-micro margin-bottom-small">
             <p class="font-size-11 mb-4">
               <?php
-              if ($custom_text) {
-                echo $custom_text;
-              } else if ($default_support_section_text) {
-                echo $default_support_section_text;
-              } else {
-                echo "Fund truthful, independent journalism. Donate one hour’s wage per month or whatever you can afford today.";
+                if ($custom_text) {
+                    echo $custom_text;
+                } else if ($default_support_section_text) {
+                    echo $default_support_section_text;
+                } else {
+                    echo "Fund truthful, independent journalism. Donate one hour’s wage per month or whatever you can afford today.";
               }
               ?>
             </p>
