2d6fefbac043825377d42160df694c8036819ef7
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2024-12-02 16:10:44 +0000

    Removed default apply info and end of day value
    - Linted files and swapped to LF rather than CRLF
    - Deleted the line asking applicants to email in their application
    - Changed the way we calculate the end of day using a Unix timestamp
    -added the end of day time stamp to the has_closed conditional instead of time()

--- page-jobs.php
+++ page-jobs.php
@@ -4,11 +4,11 @@ get_header();
 <!-- main content -->
 <main id="main-content">
 <?php
-if( have_posts() ) {
-  while( have_posts() ) {
+if ( have_posts() ) {
+  while ( have_posts() ) {
     the_post();
-    $meta = get_post_meta($post->ID);
-?>
+    $meta = get_post_meta( $post->ID );
+    ?>
   <article id="page-jobs" class="container margin-top-small margin-bottom-large">
     <div class="flex-grid-row margin-bottom-small">
       <div class="flex-grid-item flex-item-s-12">
@@ -22,38 +22,41 @@ if( have_posts() ) {
       </div>
       <div class="flex-grid-item flex-item-m-12 flex-item-l-4 flex-item-xl-5 flex-item-xxl-6">
         <?php
-          $jobs = get_posts(array(
-            	'numberposts'	=> -1,
-            	'post_type' => 'job',
-            	'orderby' => 'meta_value',
-            	'order' => 'ASC',
-            	'meta_key' => '_nm_deadline',
-            	'meta_value' => (date('U') - 86400), // today minus one day of seconds to make display inclusive of deadline day
-            	'meta_type' => 'NUMERIC',
-            	'meta_compare' => '>=',
-          ));
+        $end_of_day = strtotime( 'tomorrow midnight' ) - 1; // Calculates the Unix timestamp for the very end of the current day (23:59:59)
+        $jobs = get_posts(
+            array(
+                'numberposts'  => -1,
+                'post_type'    => 'job',
+                'orderby'      => 'meta_value',
+                'order'        => 'ASC',
+                'meta_key'     => '_nm_deadline',
+                'meta_value'   => $end_of_today,
+                'meta_type'    => 'NUMERIC',
+                'meta_compare' => '>=',
+            )
+        );
 
-          if (!empty($jobs)) {
-        ?>
+        if ( ! empty( $jobs ) ) {
+          ?>
         <h5 class="font-size-10">We are currently hiring:</h5>
         <ul>
-        <?php
-            foreach ($jobs as $job) {
-              $deadline = get_post_meta($job->ID, '_nm_deadline', true);
-        ?>
-          <li><a href="<?php echo get_permalink($job); ?>" class="ui-action-link"><?php echo $job->post_title; ?> (deadline <?php echo date('j F', $deadline); ?>)</a></li>
-        <?php
-            }
-          } else {
-        ?>
-        <h5 class="mobile-padding-bottom-small">There are currently no available positions</h5>
-        <?php
+          <?php
+          foreach ( $jobs as $job ) {
+            $deadline = get_post_meta( $job->ID, '_nm_deadline', true );
+            ?>
+          <li><a href="<?php echo get_permalink( $job ); ?>" class="ui-action-link"><?php echo $job->post_title; ?> (deadline <?php echo date( 'j F', $deadline ); ?>)</a></li>
+            <?php
           }
+        } else {
+          ?>
+        <h5 class="mobile-padding-bottom-small">There are currently no available positions</h5>
+          <?php
+        }
         ?>
       </div>
     </div>
   </article>
-<?php
+    <?php
   }
 }
 ?>
