e46d27d8cdf3a2b2c883f81532e15de060c73467
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2024-12-02 11:05:44 +0000

    Revert "composer include PHPCS packages"
    This reverts commit a7c08fec3929c80a61969e78f9c8844d0ef55f4f.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
     'Moment\\' => array($vendorDir . '/fightbulc/moment/src'),
 );
