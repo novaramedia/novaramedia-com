2df90da35a3c9839cd2e482658489a142620b34e
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-02-26 15:25:10 +0000

    Creating files
    Duplicated the content from the Podcast: Foreign Agent structure and changed the name to check I can see it on staging site

--- lib/theme-options/options-front-page.php
+++ lib/theme-options/options-front-page.php
@@ -64,6 +64,7 @@ function nm_register_front_page_options_metabox() {
       'partials/specials/banners/focus-breaking-britain' => 'Focus: Breaking Britain',
       'partials/specials/banners/focus-disability-its-political' => 'Focus: Disability: It’s Political',
       'partials/specials/banners/podcast-planet-b'       => 'Podcast: Planet B',
+      'partials/specials/banners/podcast-committed'      => 'Podcast: Committed',
   );
 
   // Get all the newsletter pages and create signup options for all with correct settings
