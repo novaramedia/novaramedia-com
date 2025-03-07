af8e887aed08585df8bf2c8843eb6d4213a7b2e4
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-03-07 16:31:55 +0000

    Initial work on the banner
    - wrong image in
    - not aligned properly

--- lib/functions-rewrites.php
+++ lib/functions-rewrites.php
@@ -1,19 +1,23 @@
 <?php
 add_action( 'init', 'novara_live_rewrites' );
 
+/**
+ * Adds rewrite rules for Novara Live category.
+ */
 function novara_live_rewrites() {
-	$novara_live_cat = get_category_by_slug('novara-live');
+  $novara_live_cat = get_category_by_slug( 'novara-live' );
 
-	if ($novara_live_cat) {
-		add_rewrite_rule(
-			'tyskysour',
-			'index.php?category_name=' . $novara_live_cat->slug,
-			'top'
-		);
+  if ( $novara_live_cat ) {
+    add_rewrite_rule(
+        'tyskysour',
+        'index.php?category_name=' . $novara_live_cat->slug,
+        'top'
+    );
 
-		add_rewrite_rule(
-			'novara-live',
-			'index.php?category_name=' . $novara_live_cat->slug,
-			'top'
-		);	}
+    add_rewrite_rule(
+        'novara-live',
+        'index.php?category_name=' . $novara_live_cat->slug,
+        'top'
+    );
+  }
 }
