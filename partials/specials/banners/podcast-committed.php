a1efdb54c4907e1dbd0bcc3ebd6f780b14c514d1
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-03-10 15:14:20 +0000

    Adjusting banner to match design

--- partials/specials/banners/podcast-committed.php
+++ partials/specials/banners/podcast-committed.php
@@ -2,50 +2,34 @@
   $local_term = get_term_by( 'slug', 'committed', 'category' );
 
 if ( $local_term ) {
-    $url = get_term_link( $local_term );
+   $url = get_term_link( $local_term );
   if ( $url ) {
     ?>
-<div class="foreign-agent-banner__backgrounded" style="background-color:rgb(254, 105, 25); overflow: hidden; position: relative">
+<div class="committed-banner__backgrounded" style="background-color:rgb(254, 105, 25); overflow: hidden; position: relative">
   <style type="text/css">
-    .fallback .foreign-agent-banner__backgrounded {
-      background-image: url(<?php echo get_bloginfo( 'stylesheet_directory' ) . '/dist/img/specials/committed/committed-background.png'; ?>);
-      background-size: cover;
-      background-position: center center;
-      background-repeat: no-repeat;
+    .fallback .committed-banner__backgrounded {
+      background-image: url(<?php echo get_bloginfo( 'stylesheet_directory' ) . '/dist/img/specials/committed/committed-background.jpg'; ?>);
     }
-    .webp .foreign-agent-banner__backgrounded {
+    .webp .committed-banner__backgrounded {
       background-image: url(<?php echo get_bloginfo( 'stylesheet_directory' ) . '/dist/img/specials/committed/committed-background.webp'; ?>);
-      background-size: cover;
-      background-position: top center;
-      background-repeat: no-repeat;
     }
-    .avif .foreign-agent-banner__backgrounded {
+    .avif .committed-banner__backgrounded {
       background-image: url(<?php echo get_bloginfo( 'stylesheet_directory' ) . '/dist/img/specials/committed/committed-background.avif'; ?>);
+    }
+    .committed-banner__backgrounded {
+      height: 267px;
       background-size: cover;
-      background-position: top center;
+      background-position: 0% 29%;
       background-repeat: no-repeat;
     }
-    .foreign-agent-banner__title {
-      max-width: 280px;
-      margin: 1rem 0;
-    }
-    .foreign-agent-banner__box {
-      border: 2px solid;
-      border-radius: 0px;
-      padding: .8rem 1rem;
-      background-color: #FFF9F5;
-    }
-    .foreign-agent-banner__caption {
-      transform: rotate(1deg);
-    }
-    .foreign-agent-banner__button {
-      transform: rotate(-2deg);
-      margin-left: 2rem
+    .committed-banner__grid-row{
+      justify-content: space-between;
     }
     .committed-banner__grid-item {
       display: flex;
       justify-content: center;
-      align-items: flex-start;
+      align-items: center;
+      padding: 0px;
     }
     .committed-banner__grid-item-left {
       justify-content: flex-start;
@@ -53,49 +37,69 @@ if ( $local_term ) {
      .committed-banner__grid-item-right {
       justify-content: flex-end;
     }
-    .committed-banner__button-container {
+    .committed-banner__container {
+      display: flex;
       flex-direction: column;
+      justify-content: center;
+      min-height: 100%;
     }
     .committed-banner__logo {
       background-image: url(<?php echo get_bloginfo( 'stylesheet_directory' ) . '/dist/img/specials/committed/committed-logo-white.png'; ?>);
       background-size: contain;
       background-repeat: no-repeat;
+      background-position: center;
     }
-
+    @media screen and (max-width: 1408px) {
+      .committed-banner__backgrounded {
+        background-position: 0% 27%;
+      }
+    }
     @media screen and (max-width: 910px) {
-      .foreign-agent-banner__title {
-        max-width: 200px;
+      .committed-banner__backgrounded {
+        background-position: 0% 26%;
       }
     }
     @media screen and (max-width: 759px) {
-      .foreign-agent-banner__caption {
-        transform: rotate(-1deg);
-        display: inline-block;
-        width: 70%;
+      .committed-banner__container {
+        justify-content: start;
       }
-      .foreign-agent-banner__button {
-        transform: rotate(1deg);
-        margin-left: 1rem
+      .committed-banner__backgrounded {
+        background-position: 50% 31%;
+        background-size: 150%;
+      }
+      .committed-banner__logo {
+        order: 1;
+        height: 80px;
+      }
+      .committed-banner__grid-item-left {
+        order: 2;
+        justify-content: flex-start;
+        align-items: flex-start;
+
+      }
+      .committed-banner__grid-item-right {
+        order: 3;
+        align-items: flex-start;
       }
     }
   </style>
-  <div class="container pt-6 pb-6">
-    <div class="grid-row">
-      <div class="grid-item is-s-24 is-l-8 is-xxl-8 mb-s-3 committed-banner__grid-item committed-banner__grid-item-left">
-        <p class="font-color-white font-weight-bold font-size-10">Would you go to prison</br>for your politics?</p>
+  <div class="container committed-banner__container pt-6 pb-6">
+    <div class="grid-row committed-banner__grid-row">
+      <div class="grid-item is-s-12 is-l-8 is-xxl-5 committed-banner__grid-item committed-banner__grid-item-left mt-s-4">
+        <p class="font-color-white font-weight-bold font-size-11 font-size-s-12">Would you go to prison</br>for your politics?</p>
       </div>
-      <div class="grid-item is-s-24 is-l-8 is-xxl-8 mb-s-3 committed-banner__grid-item committed-banner__logo">
+      <div class="grid-item is-s-24 is-xs-20 is-l-8 is-xxl-14 committed-banner__grid-item committed-banner__logo">
       </div>
-      <div class="grid-item is-s-24 is-l-8 is-xxl-8 mb-s-3 committed-banner__grid-item committed-banner__grid-item-right">
-        <div class="committed-banner__button-container">
-          <p class="font-color-white font-weight-bold mb-3">Meet the people who</br>have done just that</p>
-          <a href="<?php echo $url; ?>" class="ui-button ui-button--white">Listen Now</a>
+      <div class="grid-item is-s-12 is-l-8 is-xxl-5 committed-banner__grid-item committed-banner__grid-item-right mt-s-4">
+        <div class="committed-banner__button-container committed-banner__grid-item-right">
+          <p class="font-color-white font-weight-bold font-size-11 mb-3 pt-6 pt-s-0 font-size-s-12">Meet the people who</br>have done just that</p>
+          <a href="<?php echo $url; ?>" class="ui-button ui-button--small ui-button--white">Listen Now</a>
         </div>
       </div>
     </div>
   </div>
 </div>
-    <?php
+     <?php
   }
 }
 ?>
