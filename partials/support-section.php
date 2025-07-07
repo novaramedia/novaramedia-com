3075ba203caf6774678893f2cf561350ce0a0ca3
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-07-07 10:04:19 +0100

    Added grid row in

--- partials/support-section.php
+++ partials/support-section.php
@@ -1,6 +1,8 @@
 <div class="container">
+  <div class="flex-grid-row">
      <?php
       render_support_form_dispatcher( 'banner' );
       render_support_form_dispatcher( 'condensed' );
       ?>
+  </div>
 </div>
