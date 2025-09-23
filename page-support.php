061733301bed4288465545f4097c23d709634f76
varffs <patrickbest@patrickbest.com>
2025-09-23 19:23:16 +0300

    design iteration pass support page

--- page-support.php
+++ page-support.php
@@ -31,7 +31,7 @@ if ( have_posts() ) {
   }
   ?>
   <article id="page" class="support-page">
-    <div class="background-white background-support-texture-alt background-support-texture-alt--fade-to-white">
+    <div class="background-white background-support-texture-alt background-support-texture-alt--fade-to-gray">
       <div class="container">
         <div class="grid-item">
           <h4 class="font-size-10 font-weight-bold pt-4 pb-3 ui-border-bottom ui-border--black">
@@ -64,8 +64,8 @@ if ( have_posts() ) {
       <div class="container">
         <div class="grid-row u-relative">
           <div class="grid-item is-xxl-24">
-            <div class="grid-row grid-row--nested u-relative">
-              <div class="grid-item is-xxl-12 is-m-24 background-white support-page__left-radius">
+            <div class="grid-row grid-row--nested u-relative background-white ui-rounded-box--top">
+              <div class="grid-item is-xxl-12 is-m-24">
                 <div class="p-5 p-l-4 font-weight-bold">
                   <div class="font-size-15 font-size-l-14 mb-4">
                     Truthful, independent journalism <span class="font-color-red">funded by people like you</span>.
@@ -76,111 +76,128 @@ if ( have_posts() ) {
                   <div class="font-size-13 font-size-l-12 mb-5">
                     Be part of the change.
                   </div>
-                  <?php
-                    if ( $youtube_id ) {
-                      ?>
-                    <div class="u-video-embed-container">
-                      <iframe class="youtube-player" type="text/html" src="<?php echo generate_youtube_embed_url( $youtube_id ); ?>"></iframe>
-                    </div>
-                      <?php
-                    }
-                  ?>
                 </div>
               </div>
-              <div class="grid-item is-xxl-12 is-m-24 background-gray-base support-page__right-radius">
+              <div class="grid-item is-xxl-12 is-m-24">
                 <div class="support-page__donation-form-sticky p-5 p-l-4">
-                  <?php render_support_form( 'condensed', true ); ?>
+                  <?php render_support_form( 'condensed' ); ?>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="support-page__below-the-fold">
-      <!-- how we are funded -->
-      <div class="container">
-        <div class="grid-row mt-5 pt-5 pb-6 support-page__text-container">
-          <div class="grid-item is-xxl-24 text-align-center">
-            <h3 class="font-size-9 font-weight-bold font-style-boxed">How we are funded</h3>
-
-            <div class="support-page__big-stats mt-4 mb-5">
-              <div class="support-page__big-stat">
-                <div class="font-size-17 font-weight-bold font-color-red">84%</div>
-                <div class="font-size-13 font-size-s-11 font-weight-bold">Supporter donations</div>
-              </div>
-              <div class="support-page__big-stat">
-                <div class="font-size-17 font-weight-bold font-color-pink">16%</div>
-                <div class="font-size-13 font-size-s-11 font-weight-bold">YouTube + Merch</div>
-              </div>
-              <div class="support-page__big-stat">
-                <div class="font-size-17 font-weight-bold">0%</div>
-                <div class="font-size-13 font-size-s-11 font-weight-bold">Murdoch</div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="grid-row">
-          <div class="grid-item is-s-24 is-xxl-12">
-            <h3 class="font-size-16 font-size-l-15 font-weight-bold mb-s-4">
-              No Paywalls.<br/>
-              <span class="font-color-red">No Paymasters.</span>
-            </h3>
-          </div>
-          <div class="grid-item is-s-24 is-xxl-12 font-size-13 font-size-l-12 font-weight-bold">
-            Because the vast majority of our income is raised directly from supporters, we can be editorially independent without ever having to toe someone else’s editorial line. It’s a key principle that has <em>always</em> underpinned our funding model.
           </div>
           <div class="grid-item is-xxl-24">
-            <hr class="mt-7 mb-7"/>
-          </div>
-        </div>
-      </div>
-
-      <!-- how we spend our funds -->
-      <div class="container mb-7">
-        <div class="grid-row">
-          <div class="grid-item is-xxl-24 mb-6 text-align-center">
-            <h3 class="font-size-9 font-weight-bold font-style-boxed">How we spend our funds</h3>
-          </div>
-          <div class="grid-item is-s-24 is-xxl-10">
-            <h3 class="font-size-16 font-size-l-15 font-weight-bold mb-s-4">
-              <span class="font-color-red">Every penny</span> Novara Media makes <span class="font-color-red">goes back into our journalism</span>.
-            </h3>
-          </div>
-          <div class="grid-item offset-s-0 is-s-24 offset-xxl-2 is-xxl-12 font-size-13 font-size-l-12 font-weight-bold font-color-gray">
-            <div class="support-page__highlighter ux-highlighter">
+            <div class="background-white ui-rounded-box--bottom pl-5 pl-l-4 pr-5 pr-l-4 pb-5 pb-l-4">
             <?php
-            if ( ! empty( $how_we_spend_our_funds_lines ) ) {
-              foreach ( $how_we_spend_our_funds_lines as $index => $line_text ) {
-                echo '<div class="ux-highlighter__line">'
-                  . esc_html( $line_text )
-                  . '</div>';
-              }
-            } else {
+            if ( $youtube_id ) {
               ?>
-              <div class="ux-highlighter__line">Your support pays for the hours it takes to research and meticulously check the claims in our articles.</div>
-              <div class="ux-highlighter__line">It pays for the studio space where we film our live show.</div>
-              <div class="ux-highlighter__line">It allows us to hire key roles, like a labour movement correspondent.</div>
-              <div class="ux-highlighter__line">It helps us fight (and win) against the smears of the rightwing press.</div>
-              <div class="ux-highlighter__line">Above all, it lets us break stories and challenge the establishment in ways mainstream media just won’t.</div>
-              <?php
+              <div class="u-video-embed-container">
+                <iframe class="youtube-player" type="text/html" src="<?php echo generate_youtube_embed_url( $youtube_id ); ?>"></iframe>
+              </div>
+                <?php
             }
             ?>
             </div>
           </div>
         </div>
       </div>
+    </div>
+
+    <div class="support-page__below-the-fold background-color-gray-base">
+      <!-- how we are funded -->
+      <div class="container mt-5 mt-l-4 mb-5 mb-l-4">
+        <div class="grid-row support-page__text-container">
+          <div class="grid-item is-xxl-24 ">
+            <div class="background-red ui-rounded-box--top text-align-center p-5 p-l-4">
+              <h3 class="font-size-9 font-weight-bold font-style-boxed font-style-boxed--grey">How we are funded</h3>
+
+              <div class="support-page__big-stats mt-4 mb-5">
+                <div class="support-page__big-stat">
+                  <div class="font-weight-bold font-color-white">
+                    <span class="font-size-17">84</span><span class="font-size-15 font-size-s-14">%</span>
+                  </div>
+                  <div class="font-size-13 font-size-s-11 font-weight-bold">Supporter donations</div>
+                </div>
+                <div class="support-page__big-stat">
+                  <div class="font-weight-bold font-color-white">
+                    <span class="font-size-17">16</span><span class="font-size-15 font-size-s-14">%</span>
+                  </div>
+                  <div class="font-size-13 font-size-s-11 font-weight-bold">YouTube + Merch</div>
+                </div>
+                <div class="support-page__big-stat">
+                  <div class="font-weight-bold">
+                    <span class="font-size-17">0</span><span class="font-size-15 font-size-s-14">%</span>
+                  </div>
+                  <div class="font-size-13 font-size-s-11 font-weight-bold">Murdoch</div>
+                </div>
+              </div>
+            </div>
+            <div class="background-white ui-rounded-box--bottom p-5 p-l-4">
+              <div class="grid-row grid-row--nested">
+                <div class="grid-item is-s-24 is-xxl-12">
+                  <h3 class="font-size-16 font-size-l-15 font-weight-bold mb-s-4">
+                    No Paywalls.<br/>
+                    <span class="font-color-red">No Paymasters.</span>
+                  </h3>
+                </div>
+                <div class="grid-item is-s-24 is-xxl-12 font-size-12 font-weight-bold">
+                  <p>Because the vast majority of our income is raised directly from supporters, we can be editorially independent without ever having to toe someone else’s editorial line.</p>
+                  <p class="mt-3">It’s a key principle that has <em>always</em> underpinned our funding model.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- how we spend our funds -->
+      <div class="container mb-5">
+        <div class="grid-row">
+          <div class="grid-item is-xxl-24">
+            <div class="background-white ui-rounded-box p-5 p-l-4">
+              <div class="text-align-center mb-6">
+                <h3 class="font-size-9 font-weight-bold font-style-boxed">How we spend our funds</h3>
+              </div>
+              <div class="grid-row grid-row--nested">
+                <div class="grid-item is-s-24 is-xxl-10">
+                  <h3 class="font-size-16 font-size-l-15 font-weight-bold mb-s-4">
+                    <span class="font-color-red">Every penny</span> Novara Media makes <span class="font-color-red">goes back into our journalism</span>.
+                  </h3>
+                </div>
+                <div class="grid-item offset-s-0 is-s-24 offset-xxl-2 is-xxl-12 font-size-12 font-weight-bold font-color-gray">
+                  <div class="support-page__highlighter ux-highlighter">
+                  <?php
+                  if ( ! empty( $how_we_spend_our_funds_lines ) ) {
+                    foreach ( $how_we_spend_our_funds_lines as $index => $line_text ) {
+                      echo '<div class="ux-highlighter__line">'
+                        . esc_html( $line_text )
+                        . '</div>';
+                    }
+                  } else {
+                    ?>
+                    <div class="ux-highlighter__line">Your support pays for the hours it takes to research and meticulously check the claims in our articles.</div>
+                    <div class="ux-highlighter__line">It pays for the studio space where we film our live show.</div>
+                    <div class="ux-highlighter__line">It allows us to hire key roles, like a labour movement correspondent.</div>
+                    <div class="ux-highlighter__line">It helps us fight (and win) against the smears of the rightwing press.</div>
+                    <div class="ux-highlighter__line">Above all, it lets us break stories and challenge the establishment in ways mainstream media just won’t.</div>
+                    <?php
+                  }
+                  ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <!-- our story -->
-      <div class="container mb-8">
+      <div class="container mb-6">
         <div class="grid-row">
           <div class="grid-item is-xxl-24">
             <div class="grid-row grid-row--nested background-black">
               <div class="grid-item is-xxl-24 support-page__our-story-background ui-rounded-box--top"></div>
             </div>
-            <div class="grid-row grid-row--nested p-5 background-black font-color-white ui-rounded-box--bottom">
+            <div class="grid-row grid-row--nested p-6 p-l-5 background-black font-color-white ui-rounded-box--bottom">
               <div class="grid-item is-xxl-24 text-align-center mb-6">
                 <h3 class="font-size-9 font-weight-bold font-style-boxed font-style-boxed--white">
                   Our Story
@@ -191,7 +208,7 @@ if ( have_posts() ) {
                   Novara Media has grown from a humble radio show in 2011 to one of Britain’s most influential independent media organizations.
                 </h3>
               </div>
-              <div class="grid-item is-s-24 is-xxl-12 font-size-12 font-size-l-11 font-weight-bold">
+              <div class="grid-item is-s-24 is-xxl-12 font-size-11 font-weight-bold">
                 <p class="mb-4">
                   Born amid anti-austerity movements with nothing but passion, we've consistently punched above our weight in the national conversation.
                 </p>
@@ -226,17 +243,17 @@ if ( have_posts() ) {
     $all_quotes = array_merge( $support_carousel_quotes, array_slice( $fallback_quotes, 0, max( 0, $max_quotes - count( $support_carousel_quotes ) ) ) );
     ?>
     <!-- carousel -->
-    <section class="container support-page__quote-carousel ux-gallery-carousel mb-8" data-autoplay="true">
+    <section class="container support-page__quote-carousel ux-gallery-carousel mb-6" data-autoplay="true">
       <div class="support-page__quote-carousel-fade-left"></div>
       <div class="support-page__quote-carousel-fade-right"></div>
       <div class="swiper">
         <div class="swiper-wrapper">
           <?php foreach ( $all_quotes as $quote ) { ?>
             <div class="swiper-slide text-align-center ui-rounded-box-large">
-              <h5 class="font-weight-bold mb-5 text-uppercase font-color-black font-size-9">Supporters Say</h5>
+              <h5 class="font-size-9 font-weight-bold font-style-boxed font-style-boxed--grey">Supporters Say</h5>
               <div class="support-page__quote-container">
                 <div class="font-serif quote support-page__quote-mark text-align-center">“</div>
-                <p class="font-serif font-size-12"><?php echo esc_html( $quote ); ?></p>
+                <p class="font-serif font-size-13 font-size-s-12"><?php echo esc_html( $quote ); ?></p>
               </div>
             </div>
           <?php } ?>
@@ -248,15 +265,16 @@ if ( have_posts() ) {
     <!-- donation form -->
     <div class="container">
       <div class="grid-row">
+        <div class="grid-item is-xxl-24">
         <?php render_support_form( 'banner', true ); ?>
       </div>
     </div>
 
     <div id="other-donation-methods" class="container">
       <!-- already a supporter -->
-      <div class="grid-row mt-7">
+      <div class="grid-row mt-5">
         <div class="grid-item is-s-24 is-xxl-12 mb-4">
-          <h4 class="font-size-11 font-weight-bold mb-3">Already a supporter?</h4>
+          <h4 class="font-size-13 font-weight-bold mb-3">Already a supporter?</h4>
           <p>Are you able to increase your monthly donation? Increasing by just £2 or £3 helps strengthen funding base and makes us even more secure for the future.</p>
           <p class="mt-4"><a href="https://donate.novaramedia.com/login" class="ui-button ui-button--red ui-button--small mb-s-5">Log in to your account</a></p>
         </div>
@@ -264,7 +282,7 @@ if ( have_posts() ) {
         <!-- Other donation methods -->
         <div class="grid-item is-s-24 is-xxl-12">
           <div class="mb-5 mb-s-4">
-            <h4 class="font-size-11 font-weight-bold mb-3">Other Donation Methods</h4>
+            <h4 class="font-size-13 font-weight-bold mb-3">Other Donation Methods</h4>
             <p>The best way to ensure we receive as much of your donation as possible after processing fees is to make a payment directly through our website, however we also have options for PayPal or UK Direct Debit.</p>
           </div>
 
