8e28aa4549b794ee4623263a0ee915808638da38
varffs <patrickbest@patrickbest.com>
2025-10-17 17:37:11 +0100

    refactor signup banners for boxed background

--- partials/email-signup.php
+++ partials/email-signup.php
@@ -7,6 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Each newsletter has metadata set to make choices about how the signup block renders,
  * however there is also the ability to override some of those options by passing them in as arguments to the partial.
  * There are also defaults set for some of these options in case they are not set in the metadata.
+ * TODO: Migrate this to use the new post types.
  */
 
   $newsletter_page = get_post( $args['newsletter_page_id'] );
@@ -15,23 +16,23 @@ if ( ! $newsletter_page ) {
   return;
 }
 
-  $meta = get_post_meta( $newsletter_page->ID );
-  $mailchimp_key = ! empty( $meta['_nm_mailchimp_key'] ) ? $meta['_nm_mailchimp_key'][0] : false;
+$meta = get_post_meta( $newsletter_page->ID );
+$mailchimp_key = ! empty( $meta['_nm_mailchimp_key'] ) ? $meta['_nm_mailchimp_key'][0] : false;
 
 if ( ! $mailchimp_key ) {
   return; // if there isn't a mailchimp key then don't render the signup block
 }
 
-  // get metadata with fallback defaults
-  $background_color = ! empty( $meta['_nm_banner_background'] ) ? $meta['_nm_banner_background'][0] : 'black';
-  $text_color = ! empty( $meta['_nm_banner_text_color'] ) ? $meta['_nm_banner_text_color'][0] : 'white';
-  $button_color = ! empty( $meta['_nm_banner_button_color'] ) ? $meta['_nm_banner_button_color'][0] : 'red';
+// get metadata with fallback defaults
+$background_color = ! empty( $meta['_nm_banner_background'] ) ? $meta['_nm_banner_background'][0] : 'black';
+$text_color = ! empty( $meta['_nm_banner_text_color'] ) ? $meta['_nm_banner_text_color'][0] : 'white';
+$button_color = ! empty( $meta['_nm_banner_button_color'] ) ? $meta['_nm_banner_button_color'][0] : 'red';
 
-  $headline = ! empty( $meta['_nm_banner_headline'] ) ? $meta['_nm_banner_headline'][0] : 'Sign up to our newsletter ' . $mailchimp_key;
-  $copy = ! empty( $meta['_nm_banner_text'] ) ? $meta['_nm_banner_text'][0] : false;
-  $image_id = ! empty( $meta['_nm_banner_image_id'] ) ? $meta['_nm_banner_image_id'][0] : false;
+$headline = ! empty( $meta['_nm_banner_headline'] ) ? $meta['_nm_banner_headline'][0] : 'Sign up to our newsletter ' . $mailchimp_key;
+$copy = ! empty( $meta['_nm_banner_text'] ) ? $meta['_nm_banner_text'][0] : false;
+$image_id = ! empty( $meta['_nm_banner_image_id'] ) ? $meta['_nm_banner_image_id'][0] : false;
 
-  // override colours if set
+// override colours if set
 if ( ! empty( $args['background-color'] ) ) {
   $background_color = $args['background-color'];
 }
@@ -44,52 +45,38 @@ if ( ! empty( $args['button-color'] ) ) {
   $button_color = $args['button-color'];
 }
 
-  $hide_discover = false;
+$hide_discover = false;
 
 if ( ! empty( $args['hide-discover'] ) ) {
   $hide_discover = $args['hide-discover'];
 }
-
-  $hide_border = false;
-
-if ( ! empty( $args['hide-border'] ) ) {
-  $hide_border = $args['hide-border'];
-}
 ?>
-<div class="email-signup
-<?php
-if ( $background_color === 'white' ) {
-  echo 'mt-6 mb-5 ';
-} else {
-  echo 'pt-6 pb-6 pt-s-5 pb-s-5 '; }
-?>
-background-<?php echo $background_color; ?> font-color-<?php echo $text_color; ?>">
+<div class="email-signup mt-4 mb-4">
   <div class="container">
     <div class="grid-row">
-      <div class="grid-item is-s-24 is-l-12 is-xxl-10 mb-s-4">
-        <h3 class="font-size-15 font-size-s-12 font-weight-bold mb-4 text-wrap-pretty"><?php echo $headline; ?></h3>
-        <p class="font-size-12 font-size-s-10 font-weight-bold mr-6">
-          <?php echo $copy; ?>
-        </p>
-        <?php if ( ! $hide_discover ) { ?>
-          <div class="mt-3 font-size-8 font-weight-bold">
-            <a href="<?php echo site_url( 'newsletters/' ); ?>" class="ui-hover"><span class="ui-dot ui-dot--red"></span>Discover all our newsletters</a>
+      <div class="grid-item is-xxl-24">
+        <div class="grid-row <?php echo 'background-' . $background_color . ' font-color-' . $text_color; ?> ui-rounded-box ui-backgrounded-box-padding">
+          <div class="grid-item is-s-24 is-l-12 is-xxl-10 mb-s-4">
+            <h3 class="font-size-14 font-size-s-12 font-weight-bold mb-4 text-wrap-pretty"><?php echo $headline; ?></h3>
+            <p class="font-size-12 font-size-s-10 font-weight-bold mr-5 text-wrap-balance">
+              <?php echo $copy; ?>
+            </p>
+            <?php if ( ! $hide_discover ) { ?>
+              <div class="mt-3 font-size-8 font-weight-bold">
+                <a href="<?php echo site_url( 'newsletters/' ); ?>" class="ui-hover"><span class="ui-dot ui-dot--red"></span>Discover all our newsletters</a>
+              </div>
+            <?php } ?>
           </div>
-        <?php } ?>
-      </div>
-      <div class="grid-item offset-l-0 offset-xxl-2 <?php echo $image_id === false ? 'is-s-24 is-m-12 is-l-10 is-xxl-8' : 'is-s-16 is-xxl-8'; ?>">
-        <?php render_mailchimp_signup_form( $mailchimp_key, $background_color, $button_color ); ?>
-      </div>
-      <?php if ( $image_id ) { ?>
-        <div class="grid-item is-s-8 is-xxl-4">
-          <?php echo wp_get_attachment_image( $image_id, 'col4-square', false, array( 'class' => 'email-signup__image' ) ); ?>
+          <div class="grid-item offset-l-0 offset-xxl-2 <?php echo $image_id === false ? 'is-s-24 is-m-12 is-l-10 is-xxl-8' : 'is-s-16 is-xxl-8'; ?>">
+            <?php render_mailchimp_signup_form( $mailchimp_key, $background_color, $button_color ); ?>
+          </div>
+          <?php if ( $image_id ) { ?>
+            <div class="grid-item is-s-8 is-xxl-4">
+              <?php echo wp_get_attachment_image( $image_id, 'col4-square', false, array( 'class' => 'email-signup__image' ) ); ?>
+            </div>
+          <?php } ?>
         </div>
-      <?php } ?>
-      <?php if ( $background_color == 'white' && $hide_border !== true ) { ?>
-        <div class="grid-item is-xxl-24 mt-5">
-          <hr />
-        </div>
-      <?php } ?>
+      </div>
     </div>
   </div>
 </div>
