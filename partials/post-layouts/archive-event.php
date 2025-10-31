fd048727752f934fdfeb4c1252b62324205205a4
varffs <patrickbest@patrickbest.com>
2025-10-31 17:15:45 +0000

    simple pass update views

--- partials/post-layouts/archive-event.php
+++ partials/post-layouts/archive-event.php
@@ -1,60 +1,76 @@
 <?php
-  $timestamp = get_post_meta( $post->ID, '_cmb_time', true );
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
 
-  if ($timestamp) {
-    $time = new \Moment\Moment('@' . $timestamp);
-  } else {
-    $time = new \Moment\Moment('@' . get_the_time('U'));
-  }
+$timestamp = get_post_meta( $post->ID, '_cmb_time', true );
 
-  $venue_name = get_post_meta( $post->ID, '_cmb_venue_name', true );
-  $speakers = get_post_meta( $post->ID, '_cmb_speakers', true );
-  $host = get_post_meta( $post->ID, '_cmb_host', true );
+if ( $timestamp ) {
+  $time = new \Moment\Moment( '@' . $timestamp );
+} else {
+  $time = new \Moment\Moment( '@' . get_the_time( 'U' ) );
+}
+
+$venue_name = get_post_meta( $post->ID, '_cmb_venue_name', true );
+$venue_postcode = get_post_meta( $post->ID, '_cmb_venue_postcode', true );
+$speakers = get_post_meta( $post->ID, '_cmb_speakers', true );
+$host = get_post_meta( $post->ID, '_cmb_host', true );
 ?>
-
-<a href="<?php the_permalink(); ?>">
-  <div class="row margin-bottom-mid">
-    <div class="col col1"></div>
-    <div class="col col9">
-      <?php the_post_thumbnail('col12'); ?>
-    </div>
-    <div class="col col1"></div>
-    <div class="col col13">
-      <h3 class="margin-bottom-tiny"><?php echo $time->format('j F Y'); ?>:</h3>
-      <h2 class="margin-bottom-tiny"><?php the_title(); ?></h2>
+<div class="grid-row mb-5">
+  <div class="grid-item is-xxl-10 is-s-24 mb-s-2 text-align-center">
+    <a href="<?php the_permalink(); ?>" class="ui-hover">
       <?php
-        if ($venue_name) {
+      the_post_thumbnail(
+        array( 600, 400 ),
+        array(
+          'alt'   => get_the_title(),
+          'class' => 'ui-rounded-image',
+        )
+      );
       ?>
-        <h3>At <?php echo $venue_name; ?></h3>
-      <?php
-        }
-      ?>
-      <?php
-        if ($speakers) {
-      ?>
-        <h3>With <?php
-          $i = 0;
-          $last = count($speakers) - 1;
-          foreach ($speakers as $speaker) {
-            if ($i === $last) {
-              echo ' & ';
-            } else if ($i > 0) {
-              echo ', ';
-            }
-            echo $speaker;
-            $i++;
-          }
-        ?></h3>
-      <?php
-        }
-      ?>
-      <?php
-        if ($host) {
-      ?>
-        <h3>Hosted by <?php echo $host; ?></h3>
-      <?php
-        }
-      ?>
-    </div>
+    </a>
   </div>
-</a>
+  <div class="grid-item is-xxl-14 is-s-24">
+    <a href="<?php the_permalink(); ?>" class="ui-hover">
+      <h3 class="font-weight-bold mb-2"><?php echo $time->format( 'j F Y' ); ?>:</h3>
+      <h2 class="font-size-13 font-weight-bold text-wrap-balance mb-3"><?php the_title(); ?></h2>
+    </a>
+    <?php
+    if ( $venue_name ) {
+      ?>
+      <a href="https://www.google.com/maps/search/<?php echo $venue_postcode; ?>" target="_blank" rel="nofollow" class="ui-hover">
+        <h3 class="font-size-11 font-weight-bold mb-3">At <?php echo $venue_name; ?></h3>
+      </a>
+      <?php
+    }
+    ?>
+    <?php
+    if ( $speakers ) {
+      ?>
+      <h3 class="font-size-12 font-weight-bold mb-1">With
+      <?php
+      $i = 0;
+      $number_of_speakers = count( $speakers );
+      foreach ( $speakers as $speaker ) {
+        ++$i;
+        if ( $i === $number_of_speakers && $number_of_speakers > 1 ) {
+          echo ' & ';
+        } elseif ( $i > 1 ) {
+          echo ', ';
+        }
+        echo $speaker;
+      }
+      ?>
+      </h3>
+      <?php
+    }
+    ?>
+    <?php
+    if ( $host ) {
+      ?>
+      <h3 class="font-size-12 font-weight-bold">Hosted by <?php echo $host; ?></h3>
+      <?php
+    }
+    ?>
+  </div>
+</div>
--- single-event.php
+++ single-event.php
@@ -1,21 +1,25 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
 get_header();
 ?>
 <main id="main-content">
 <?php
-if( have_posts() ) {
-  while( have_posts() ) {
+if ( have_posts() ) {
+  while ( have_posts() ) {
     the_post();
 
     $timestamp = get_post_meta( $post->ID, '_cmb_time', true );
 
-    if ($timestamp) {
-      $time = new \Moment\Moment('@' . $timestamp);
+    if ( $timestamp ) {
+      $time = new \Moment\Moment( '@' . $timestamp );
     } else {
-      $time = new \Moment\Moment('@' . get_the_time('U'));
+      $time = new \Moment\Moment( '@' . get_the_time( 'U' ) );
     }
 
-    $fromEvent = $time->fromNow();
+    $time_from_event = $time->fromNow();
 
     $venue_name = get_post_meta( $post->ID, '_cmb_venue_name', true );
     $venue_postcode = get_post_meta( $post->ID, '_cmb_venue_postcode', true );
@@ -29,97 +33,104 @@ if( have_posts() ) {
     $youtube_id = get_post_meta( $post->ID, '_cmb_youtube', true );
 
     $gallery = get_post_meta( $post->ID, '_cmb_gallery', true );
-?>
+    ?>
   <article id="event">
-    <div class="container mt-4 mb-6">
+    <div class="container">
       <div class="grid-row mb-4">
         <div class="grid-item is-xxl-24">
-          <h4 class="font-size-9 text-uppercase font-weight-bold">Events</h4>
+          <h4 class="font-size-10 font-weight-bold pt-4 pb-3 ui-border-bottom ui-border--black">
+            <a href="<?php echo esc_url( get_post_type_archive_link( 'event' ) ); ?>" class="ui-hover">
+              Event
+            </a>
+          </h4>
+        </div>
+      </div>
+      <div class="grid-row mb-4">
+        <div class="grid-item is-xxl-24 text-align-center">
+          <h3 class="font-size-10 font-weight-bold mb-2"><?php echo $time->format( 'j F Y' ); ?></h3>
+          <h1 class="font-size-15 font-weight-bold text-wrap-balance"><?php the_title(); ?></h1>
         </div>
       </div>
       <div class="grid-row mb-5">
-        <div class="grid-item is-xxl-24 text-align-center">
-          <h1 class="font-size-15 font-weight-bold"><?php echo $time->format('j F Y'); ?>: <?php the_title(); ?></h1>
-        </div>
-      </div>
-      <div class="grid-row mb-4">
         <?php
-          if ($youtube_id) {
-        ?>
+        if ( $youtube_id ) {
+          ?>
         <div class="grid-item offset-s-0 is-s-24 offset-xxl-4 is-xxl-16">
           <div class="u-video-embed-container">
-            <iframe class="youtube-player lazyload" data-src="<?php echo generate_youtube_embed_url($youtube_id); ?>" frameborder="0" allowfullscreen></iframe>
+            <iframe class="youtube-player lazyload" data-src="<?php echo generate_youtube_embed_url( $youtube_id ); ?>" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
-        <?php
-          } else {
-        ?>
-        <div class="grid-item offset-s-2 is-s-20 offset-l-3 is-l-18 offset-xxl-6 is-xxl-12">
-          <?php the_post_thumbnail('is-xxl-12'); ?>
+          <?php
+        } else {
+          ?>
+        <div class="grid-item is-xxl-24 text-align-center">
+          <?php the_post_thumbnail( array( 600, 500 ) ); ?>
         </div>
-        <?php
-          }
+          <?php
+        }
         ?>
       </div>
       <div class="grid-row mb-4">
         <div class="grid-item offset-s-0 is-s-8 offset-xxl-2 is-xxl-8">
           <div class="mb-4">
             <h5 class="font-size-10 font-weight-bold mb-2">Time:</h5>
-            <h3 class="font-size-11 font-weight-bold"><?php echo $time->format('j'); ?><sup><?php echo $time->format('S'); ?></sup><?php echo $time->format(' F Y'); ?></h3>
-            <h3 class="font-size-11 font-weight-bold"><?php echo $time->format('H:i'); ?></h3>
+            <h3 class="font-size-12 font-weight-bold"><?php echo $time->format( 'j F Y' ); ?></h3>
+            <h3 class="font-size-12 font-weight-bold"><?php echo $time->format( 'H:i' ); ?></h3>
           </div>
         <?php
-          if ($venue_name) {
-        ?>
-          <div class="mb-4">
-            <h5 class="font-size-10 font-weight-bold mb-2">Venue:</h5>
-            <h3><?php echo $venue_name; ?></h3>
-        <?php
-            if ($venue_postcode) {
-        ?>
-            <h3 class="font-size-11 font-weight-bold"><a href="https://www.google.com/maps/search/<?php echo urlencode($venue_postcode); ?>" target="_blank" rel="nofollow"><?php echo $venue_postcode; ?></a></h3>
-        <?php
-            }
-        ?>
-          </div>
-        <?php
+        if ( $venue_name ) {
+          ?>
+        <div class="mb-4">
+          <h5 class="font-size-10 font-weight-bold mb-2">Venue:</h5>
+          <h3 class="font-size-12 font-weight-bold"><?php echo $venue_name; ?></h3>
+          <?php
+          if ( $venue_postcode ) {
+            ?>
+          <h3 class="font-size-11 font-weight-bold mt-1">
+            <a href="<?php echo esc_url( 'https://www.google.com/maps/search/' . $venue_postcode ); ?>" target="_blank" rel="nofollow"><?php echo $venue_postcode; ?></a>
+          </h3>
+            <?php
           }
+          ?>
+        </div>
+          <?php
+        }
 
-          if ($speakers) {
-        ?>
-          <div class="mb-4">
-            <h5 class="font-size-10 font-weight-bold mb-2">Speakers:</h5>
-        <?php
-          foreach ($speakers as $speaker) {
-        ?>
-            <h3 class="font-size-11 font-weight-bold"><?php echo $speaker; ?></h3>
-        <?php
+        if ( $speakers && is_array( $speakers ) && count( $speakers ) > 0 ) {
+          ?>
+        <div class="mb-4">
+          <h5 class="font-size-10 font-weight-bold mb-2">Speakers:</h5>
+          <?php
+          foreach ( $speakers as $speaker ) {
+            ?>
+          <h3 class="font-size-12 font-weight-bold"><?php echo $speaker; ?></h3>
+            <?php
           }
-        ?>
-          </div>
-        <?php
-          }
+          ?>
+        </div>
+          <?php
+        }
 
-          if ($host) {
-        ?>
-          <div class="mb-4">
-            <h5 class="font-size-10 font-weight-bold mb-2">Host:</h5>
-            <h3 class="font-size-11 font-weight-bold"><?php echo $host; ?></h3>
-          </div>
-        <?php
-          }
+        if ( $host ) {
+          ?>
+        <div class="mb-4">
+          <h5 class="font-size-10 font-weight-bold mb-2">Host:</h5>
+          <h3 class="font-size-12 font-weight-bold"><?php echo $host; ?></h3>
+        </div>
+          <?php
+        }
 
-          if ($is_sold_out) {
-        ?>
-            <h4 class="font-size-10 font-weight-bold">Sold Out!</h4>
-        <?php
-          }
+        if ( $is_sold_out ) {
+          ?>
+          <h4 class="font-size-12 font-weight-bold">Sold Out!</h4>
+          <?php
+        }
 
-          if (!$is_sold_out && $tickets_url && $fromEvent->getDirection() !== 'past') {
-        ?>
-            <a href="<?php echo $tickets_url; ?>" target="_blank" rel="nofollow" class="ui-button ui-button--black"><h4>Buy Tickets</h4></a>
-        <?php
-          }
+        if ( ! $is_sold_out && $tickets_url && $time_from_event->getDirection() !== 'past' ) {
+          ?>
+          <a href="<?php echo $tickets_url; ?>" target="_blank" rel="nofollow" class="ui-button ui-button--red"><h4>Buy Tickets</h4></a>
+          <?php
+        }
         ?>
         </div>
         <div class="grid-item is-s-16 is-xxl-12 text-copy font-size-10 font-serif">
@@ -128,8 +139,8 @@ if( have_posts() ) {
       </div>
     </div>
     <?php
-      if ($gallery) {
-    ?>
+    if ( $gallery ) {
+      ?>
     <div class="background-black font-grid-itemor-white padding-top-mid padding-bottom-mid">
       <div class="container">
         <div class="grid-row mb-4">
@@ -139,26 +150,27 @@ if( have_posts() ) {
         </div>
         <div class="grid-row mb-4">
           <div class="grid-item is-xxl-24">
-            <?php echo do_shortcode($gallery); ?>
+            <?php echo do_shortcode( $gallery ); ?>
           </div>
         </div>
       </div>
     </div>
-    <?php
-      }
+      <?php
+    }
     ?>
   </article>
-<?php
+    <?php
   }
 } else {
-?>
+  ?>
   <article id="event" class="container mb-4">
     <div class="grid-row">
-      <article class="grid-item is-xxl-24"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+      <article class="grid-item is-xxl-24">Sorry, no posts matched your criteria :{</article>
     </div>
   </article>
-<?php
-} ?>
+    <?php
+}
+?>
   <?php
     get_template_part( 'partials/support-section', null, array( 'container_classes' => 'mb-4' ) );
   ?>
