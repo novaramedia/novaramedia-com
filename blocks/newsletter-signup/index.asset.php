cae29289c40c25534775c4935c3a6165c189cfbc
varffs <patrickbest@patrickbest.com>
2026-02-09 22:02:33 +0000

    Build

--- blocks/newsletter-signup/index.asset.php
+++ blocks/newsletter-signup/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8b5a11d29fd8b43138a8');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '917a6a447aed9ed1489d');
